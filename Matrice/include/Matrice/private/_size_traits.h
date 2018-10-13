186562c0608c3366f98182a0e6725e78b35535eb
Zhilong Su <su-zl@seu.edu.cn>
2018-10-13 15:41:50 +0800

    Add size traits.

--- Matrice/include/Matrice/private/_size_traits.h
+++ Matrice/include/Matrice/private/_size_traits.h
@@ -19,4 +19,31 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 #include"_type_traits.h"
 
+DGE_MATRICE_BEGIN
+template<int _Rows = 0, int _Cols = 0> struct compile_time_size {
+	enum { val_1 = 0x0001, val_2 = 0x0002, val_3 = 0x0003, val_4 = 0x0004 };
+	enum { CompileTimeRows = _Rows,  CompileTimeCols = _Cols,
+			 RunTimeDeducedOnHost = 0, RunTimeDeducedOnDevice = -1 };
+};
+// \compile-time size of row value
+template<int _M, int _N> MATRICE_GLOBAL_INL constexpr int ct_size_rv = compile_time_size<_M, _N>::CompileTimeRows;
+// \compile-time size of column value
+template<int _M, int _N> MATRICE_GLOBAL_INL constexpr int ct_size_cv = compile_time_size<_M, _N>::CompileTimeCols;
+// \statically deduced runtime-size on host
+template<int _M, int _N> MATRICE_GLOBAL_INL constexpr int rs_host_v = compile_time_size<_M, _N>::RunTimeDeducedOnHost;
+// \statically deduced runtime-size on device
+template<int _M, int _N> MATRICE_GLOBAL_INL constexpr int rs_device_v = compile_time_size<_M, _N>::RunTimeDeducedOnDevice;
 
+
+template<bool _Test, int _N1, int _N2> struct conditional_size {};
+template<int _N1, int _N2 > struct conditional_size<std::true_type::value, _N1, _N2> { enum { value = _N1 }; };
+template<int _N1, int _N2 > struct conditional_size<std::false_type::value, _N1, _N2> { enum { value = _N2 }; };
+template<bool _Test, int _N1, int _N2> MATRICE_GLOBAL_INL constexpr int conditional_size_v = conditional_size<_Test, _N1, _N2>::value;
+
+
+template<int _N1, int _N2> struct max_integer { enum { value = conditional_size_v<(_N1 > _N2), _N1, _N2> }; };
+template<int _N1, int _N2> MATRICE_GLOBAL_INL constexpr int max_integer_v = max_integer<_N1, _N2>::value;
+template<int _N1, int _N2> struct min_integer { enum { value = conditional_size_v<(_N1 > _N2), _N2, _N1> }; };
+template<int _N1, int _N2> MATRICE_GLOBAL_INL constexpr int min_integer_v = min_integer<_N1, _N2>::value;
+
+DGE_MATRICE_END
