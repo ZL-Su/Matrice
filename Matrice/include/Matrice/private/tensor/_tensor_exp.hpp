d56d0d6f2bac1e99fa68ceeb3d0ab9d83475b9b4
Zhilong Su <su-zl@seu.edu.cn>
2018-11-15 16:26:20 +0800

    Update dgelom::_Tensor_exp type.

--- Matrice/include/Matrice/private/tensor/_tensor_exp.hpp
+++ Matrice/include/Matrice/private/tensor/_tensor_exp.hpp
@@ -18,20 +18,27 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 #pragma once
 #include "../_matrix_exp.hpp"
+#include "../_range.h"
 
 DGE_MATRICE_BEGIN namespace detail {
 struct _Tensor_exp_op {
-	template<typename... _Args>
-	using _Ewise_mmul = _Exp_op::_Mat_mul<_Args...>;
+	template<typename T, typename U>
+	using _Ewise_mmul = Expr::MatBinaryExpr<T, U, _Exp_op::_Mat_mul<common_value_t<T, U>>>;
 };
 
 template<typename _Derived> class _Tensor_exp_base {
-
+	using _Mytraits = tensor_traits<_Derived>;
+public:
+	MATRICE_GLOBAL_INL auto reduce() const {
+		auto _Derived_this = static_cast<const _Derived*>(this);
+		auto _Ret = (*_Derived_this)(0).eval();
+		for (const auto _Idx : range(1, M*K))
+			_Ret = _Ret + (*_Derived_this)(_Idx);
+		return (_Ret);
+	}
 protected:
 	std::size_t M, K, N;
 };
-
-template<typename T, typename U, typename _Op> class _Tensor_exp {};
 
 template<typename T, typename U, typename _Op,
 	typename = std::enable_if_t<is_tensor_v<T>&&is_tensor_v<U>>>
@@ -39,19 +46,19 @@ class _Tensor_exp : public _Tensor_exp_base<_Tensor_exp<T,U,_Op>> {
 	using _Mybase = _Tensor_exp_base<_Tensor_exp<T, U, _Op>>;
 public:
 	_Tensor_exp(const T& _Left, const U& _Right) 
-		: _LHS(_Left), _RHS(_Right) {}
+		: _LHS(_Left), _RHS(_Right) {
+		_Mybase::M = _LHS.rows();
+		_Mybase::K = _LHS.cols();
+		_Mybase::N = _LHS.cols();
+	}
 
 	MATRICE_GLOBAL_INL auto operator() (std::size_t _Idx) const {
-		return _Myop(_LHS(_Idx), _RHS(_Idx));
+		return _Op(_LHS(_Idx), _RHS(_Idx));
 	}
 
 private:
-	_Op _Myop;
 	const T& _LHS;
 	const U& _RHS;
-	_Mybase::M = _LHS.rows();
-	_Mybase::K = _LHS.cols();
-	_Mybase::N = _LHS.cols();
 };
 
 } 
