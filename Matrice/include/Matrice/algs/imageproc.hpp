43413e5f5cb4f69b82fc2bfd93812d936692754b
Zhilong Su <su-zl@seu.edu.cn>
2018-11-16 23:01:56 +0800

    Add template class for image gradient computation.

--- Matrice/include/Matrice/algs/imageproc.hpp
+++ Matrice/include/Matrice/algs/imageproc.hpp
@@ -41,4 +41,63 @@ void _Grayscale_stretch(_Pixty* Img, std::size_t rows, std::size_t cols)
 		*_Begin = pixel_t(_Scal * (*_Begin - _Min));
 }
 
+namespace types {
+template<typename _Ty, int _M, int _N> class Matrix_;
+}
+
+enum {SOBEL = 0, BSPL3 = 3, BSPL5 = 5, BSPL7 = 7};
+
+namespace detail {
+template<typename _Ty, int _M, int _N> class _Multi_matrix;
+
+template<typename _Ty> struct gradient_traits {};
+
+template<typename _Ty, std::size_t _Op> class _Gradient_impl;
+template<typename _Ty, std::size_t _Op> 
+struct gradient_traits<_Gradient_impl<_Ty, _Op>> {
+	using value_type = _Ty;
+	static constexpr auto op_value = _Op;
+	using image_type = types::Matrix_<unsigned char, 0, 0>;
+	using matrix_type = types::Matrix_<value_type, 0, 0>;
+};
+
+template<typename _Derived> class _Gradient_base {
+	using _Mytraits = gradient_traits<_Derived>;
+public:
+	using value_type = typename _Mytraits::value_type;
+	using image_type = typename _Mytraits::image_type;
+	using matrix_type = typename _Mytraits::matrix_type;
+
+	_Gradient_base(const image_type& _Image) : _Myimg(_Image) {}
+
+
+	/**
+	 * \get image gradient with respective to _Axis
+	 * \Template param _Axis: 0 for x axis, 1 for y axis
+	 */
+	template<std::size_t _Axis> auto& get() {
+		return (_Mygrad[_Axis]);
+	}
+	template<std::size_t _Axis> const auto& get() const {
+		return (_Mygrad[_Axis]);
+	}
+
+private:
+	const image_type& _Myimg;
+	_Multi_matrix<value_type, 0, 0> _Mygrad;
+};
+
+template<typename _Ty>
+class _Gradient_impl<_Ty, SOBEL> 
+	: public _Gradient_base<_Gradient_impl<_Ty, SOBEL>> {
+	using _Myt = _Gradient_impl;
+	using _Mybase = _Gradient_base<_Myt>;
+public:
+	using typename _Mybase::image_type;
+	_Gradient_impl(const image_type& _Image) : _Mybase(_Image) {}
+
+
+
+};
+
 }
