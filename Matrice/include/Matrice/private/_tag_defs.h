2c928ed1f5d28d4fea69e7332acc01460e78bcc3
Zhilong Su <su-zl@seu.edu.cn>
2018-11-25 14:38:12 +0800

    Modify _Ewise_sqt_tag to _Ewise_sqrt_tag.

--- Matrice/include/Matrice/private/_tag_defs.h
+++ Matrice/include/Matrice/private/_tag_defs.h
@@ -19,30 +19,43 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 #include "../util/_macros.h"
 
-DGE_MATRICE_BEGIN namespace tag {
+DGE_MATRICE_BEGIN
+/**********************************************************************
+	This module refers to tag definitions to construct footstone of 
+	template specialization and function overloading.
+ **********************************************************************/
+namespace tag {
+	///<brief> tag definitions for categorizing matrix instance </brief>
 	struct _Matrix_tag {};
 	struct _Managed_matrix_tag : _Matrix_tag {};
 	struct _Dynamic_matrix_tag : _Matrix_tag {};
 	struct _Device_matrix_tag : _Matrix_tag {};
 
+	///<brief> tag definitions for categorizing matrix iterator </brief>
 	struct _Matrix_iterator_tag {};
 	struct _Matrix_rwise_iterator_tag {};
 	struct _Matrix_cwise_iterator_tag {};
 
+	///<brief> tag definitions for categorizing matrix view </brief>
 	struct _Matrix_view_tag {};
 	struct _Matrix_row_view_tag : _Matrix_view_tag {};
 	struct _Matrix_col_view_tag : _Matrix_view_tag {};
 	struct _Matrix_block_view_tag : _Matrix_view_tag {};
 
+	///<brief> tag definitions for categorizing expression </brief>
 	struct _Expression_tag {};
-	struct _Ewise_add_tag : _Expression_tag {};
-	struct _Ewise_sub_tag : _Expression_tag {};
-	struct _Ewise_mul_tag : _Expression_tag {};
-	struct _Ewise_div_tag : _Expression_tag {};
-	struct _Ewise_exp_tag : _Expression_tag {};
-	struct _Ewise_log_tag : _Expression_tag {};
-	struct _Ewise_sin_tag : _Expression_tag {};
-	struct _Ewise_cos_tag : _Expression_tag {};
-	struct _Ewise_sqr_tag : _Expression_tag {};
-	struct _Ewise_sqt_tag : _Expression_tag {};
-} DGE_MATRICE_END
+	struct _Ewise_add_tag : _Expression_tag {}; // $x_i + y_i$
+	struct _Ewise_sub_tag : _Expression_tag {}; // $x_i - y_i$
+	struct _Ewise_mul_tag : _Expression_tag {}; // $x_i * y_i$
+	struct _Ewise_div_tag : _Expression_tag {}; // $x_i / y_i$
+	struct _Ewise_exp_tag : _Expression_tag {}; // $ e^{x_i} $
+	struct _Ewise_log_tag : _Expression_tag {}; // $ log(x_i)$
+	struct _Ewise_sin_tag : _Expression_tag {}; // $ sin(x_i)$
+	struct _Ewise_cos_tag : _Expression_tag {}; // $ cos(x_i)$
+	struct _Ewise_sqr_tag : _Expression_tag {}; // $ {x_i}^2 $
+	struct _Ewise_sqrt_tag : _Expression_tag {}; // $ \sqrt{x_i} $
+	struct _Matrix_mul_tag : _Expression_tag {};// matrix mul.
+	struct _Matrix_inv_tag : _Expression_tag {};// matrix inverse
+	struct _Matrix_trp_tag : _Expression_tag {};// matrix transpose
+} 
+DGE_MATRICE_END
