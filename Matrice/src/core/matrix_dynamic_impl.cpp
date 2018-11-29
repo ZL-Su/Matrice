5811ddbf74fd0fadfba3e5ebe6ff427039184dd6
Zhilong Su <su-zl@seu.edu.cn>
2018-11-29 13:12:42 +0800

    Fix bugs.

--- Matrice/src/core/matrix_dynamic_impl.cpp
+++ Matrice/src/core/matrix_dynamic_impl.cpp
@@ -4,10 +4,10 @@ MATRICE_NAMESPACE_BEGIN_TYPES
 template<typename _Ty>
 void Matrix_<_Ty, 0, 0>::create(int_t rows, int_t cols)
 {
-	base_t::m_storage = std::move(detail::Storage_<value_t>::Allocator<0, 0>(rows, cols));
-	m_rows = rows, m_cols = cols;
-	m_data = internal::_Proxy_checked(base_t::m_storage.data());
-	base_t::base_t::_Flush_view_buf();
+	_Mybase::m_storage = std::move(detail::Storage_<value_t>::Allocator<0, 0>(rows, cols));
+	_Mybase::m_rows = rows, _Mybase::m_cols = cols;
+	_Mybase::m_data = internal::_Proxy_checked(_Mybase::m_storage.data());
+	_Mybase::base_t::_Flush_view_buf();
 }
 template void Matrix_<int, 0, 0>::create(int_t, int_t);
 template void Matrix_<char, 0, 0>::create(int_t, int_t);
@@ -18,11 +18,11 @@ template void Matrix_<unsigned char, 0, 0>::create(int_t, int_t);
 template<typename _Ty>
 void Matrix_<_Ty, 0, 0>::create(int_t rows, int_t cols, value_t _val)
 {
-	base_t::m_storage = std::move(detail::Storage_<value_t>::Allocator<0, 0>(rows, cols));
-	base_t::m_storage = { _val };
-	m_rows = rows, m_cols = cols;
-	m_data = internal::_Proxy_checked(base_t::m_storage.data());
-	base_t::base_t::_Flush_view_buf();
+	_Mybase::m_storage = std::move(detail::Storage_<value_t>::Allocator<0, 0>(rows, cols));
+	_Mybase::m_storage = { _val };
+	_Mybase::m_rows = rows, _Mybase::m_cols = cols;
+	_Mybase::m_data = internal::_Proxy_checked(_Mybase::m_storage.data());
+	_Mybase::base_t::_Flush_view_buf();
 }
 template void Matrix_<int, 0, 0>::create(int_t, int_t, value_t);
 template void Matrix_<char, 0, 0>::create(int_t, int_t, value_t);
