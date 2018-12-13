e230271ecbbfcde94675de2d263e0e4a47af2cff
Zhilong Su <su-zl@seu.edu.cn>
2018-12-13 16:00:39 +0800

    Add expression module.
    To construct expression system for autograd evaluation.

---
+++ Matrice/include/Matrice/private/autograd/_expression.h
@@ -0,0 +1,45 @@
+/*********************************************************************
+This file is part of Matrice, an effcient and elegant C++ library.
+Copyright(C) 2018, Zhilong(Dgelom) Su, all rights reserved.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+#pragma once
+#include "../_matrix_exp.hpp"
+
+///<brief> construct computational expression system </brief>
+///<method> exp::forward(): for expression evaluation </method>
+///<method> exp::backward(): for derivatives computation based on chain rule </method>
+
+DGE_MATRICE_BEGIN namespace exp {
+_DETAIL_BEGIN
+
+template<typename _Derived> class _Expression_base
+{
+public:
+	_Expression_base() {};
+	~_Expression_base() {};
+
+	MATRICE_GLOBAL_INL auto forward() const {
+
+	}
+	MATRICE_GLOBAL_INL auto backward() const {
+
+	}
+private:
+
+};
+
+_DETAIL_END
+} DGE_MATRICE_END
