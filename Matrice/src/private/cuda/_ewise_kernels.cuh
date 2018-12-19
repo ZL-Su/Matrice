7581762bdd54e678446748c3d9d1a4bffaa9960c
Zhilong Su <su-zl@seu.edu.cn>
2018-12-19 16:10:46 +0800

    Update ewise kernels.

--- Matrice/src/private/cuda/_ewise_kernels.cuh
+++ Matrice/src/private/cuda/_ewise_kernels.cuh
@@ -7,9 +7,12 @@
 #include <thrust\device_vector.h>
 
 MATRICE_DEVICE_BEGIN namespace kernels {
-template<typename _Scalar, typename = std::enable_if_t<std::is_scalar_v<_Scalar>>> 
-__global__ void _Ewise_reduce(const _Scalar* _Data, std::size_t _Size, _Scalar& _Ret) {
-	_Ret = _Scalar(0);
+
+/**
+ * summation over _Data[0 : _Size - 1]
+ */
+template<typename _Ty> __global__ 
+void _Ewise_reduce(_Ty& _Ret, const _Ty* _Data, std::size_t _Size) {
 	const auto _Idx = blockIdx.x*blockDim.x + threadIdx.x;
 	if (_Idx < _Size) _Ret += _Data[_Idx];
 }
@@ -17,8 +20,8 @@ __global__ void _Ewise_reduce(const _Scalar* _Data, std::size_t _Size, _Scalar& 
 /**
  * $Z_i = X_i + Y_i$
  */
-template<typename _Scalar> __global__ void
-_Ewise_add(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size) {
+template<typename _Ty> __global__ 
+void _Ewise_add(const _Ty* _X, const _Ty* _Y, _Ty* _Z, std::size_t _Size) {
 	const auto _Idx = blockIdx.x*blockDim.x + threadIdx.x;
 	if (_Idx < _Size) {
 		_Z[_Idx] = _X[_Idx] + _Y[_Idx];
@@ -27,8 +30,8 @@ _Ewise_add(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size)
 /**
  * $Z_i = X_i - Y_i$
  */
-template<typename _Scalar> __global__ void
-_Ewise_sub(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size) {
+template<typename _Ty> __global__ 
+void _Ewise_sub(const _Ty* _X, const _Ty* _Y, _Ty* _Z, std::size_t _Size) {
 	const auto _Idx = blockIdx.x*blockDim.x + threadIdx.x;
 	if (_Idx < _Size) {
 		_Z[_Idx] = _X[_Idx] - _Y[_Idx];
@@ -37,8 +40,8 @@ _Ewise_sub(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size)
 /**
  * $Z_i = X_i * Y_i$
  */
-template<typename _Scalar> __global__ void
-_Ewise_mul(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size) {
+template<typename _Ty> __global__ 
+void _Ewise_mul(const _Ty* _X, const _Ty* _Y, _Ty* _Z, std::size_t _Size) {
 	const auto _Idx = blockIdx.x*blockDim.x + threadIdx.x;
 	if (_Idx < _Size) {
 		_Z[_Idx] = _X[_Idx] * _Y[_Idx];
@@ -47,8 +50,8 @@ _Ewise_mul(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size)
 /**
  * $Z_i = X_i / Y_i$
  */
-template<typename _Scalar> __global__ void
-_Ewise_div(const _Scalar* _X, const _Scalar* _Y, _Scalar* _Z, std::size_t _Size) {
+template<typename _Ty> __global__ 
+void _Ewise_div(const _Ty* _X, const _Ty* _Y, _Ty* _Z, std::size_t _Size) {
 	const auto _Idx = blockIdx.x*blockDim.x + threadIdx.x;
 	if (_Idx < _Size) {
 		_Z[_Idx] = _X[_Idx] / _Y[_Idx];
