6d19aa866e812f7bb04ae7a2d0f378d877b4c19e
Zhilong Su <su-zl@seu.edu.cn>
2018-12-31 15:22:14 +0800

    Update dgelom::io::detail::_Dir_impl.
    Add full path accessor: operator[];
    Add folder name getter: folder(_Idx);
    Add method size() to get number of folders.

--- Matrice/include/Matrice/io/directory.h
+++ Matrice/include/Matrice/io/directory.h
@@ -17,6 +17,8 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 #pragma once
 #include<experimental/filesystem>
+#include <unordered_map>
+#include <vector>
 #include "../util/_macros.h"
 
 DGE_MATRICE_BEGIN namespace io {
@@ -25,24 +27,72 @@ DGE_MATRICE_BEGIN namespace io {
  */
 namespace fs = std::experimental::filesystem;
 
-namespace detail {
+_DETAIL_BEGIN
 
 struct folder_tag {};
 
 template<typename _Tag = folder_tag> class _Dir_impl {};
 
 template<> class _Dir_impl<folder_tag> {
-	using value_type = std::string_view;
+	using value_type = std::string;
 	using path_type = fs::path;
+	using container = std::vector<value_type>;
 public:
-	_Dir_impl(value_type _Root, path_type&& _Workdir = fs::current_path()) {
+	/**
+	 * \_Root must has form "./folder_name" or "/folder_name" 
+	 */
+	_Dir_impl(value_type _Root, path_type&& _Workdir = fs::current_path())
+		:_Mypath(_Workdir) {
+		if (_Root[0] == '.') _Mypath.concat(_Root.begin() + 1, _Root.end());
+		else _Mypath.concat(_Root.begin(), _Root.end());
 
+		_Ext_subfolder();
+	}
+
+	/**
+	 * \return full path of _Idx-th folder
+	 */
+	MATRICE_HOST_INL auto operator[] (std::size_t _Idx) const {
+		return (_Mypath.string() + folder(_Idx));
+	}
+
+	/**
+	 * \return _Idx-th folder name under work-path _Mypath
+	 */
+	MATRICE_HOST_INL value_type folder(std::size_t _Idx) const {
+#ifdef _DEBUG
+		if (_Idx >= _Mysubfolders.size()) throw
+			std::exception("_Idx over range of _Mysubfolders");
+#endif
+		return (_Mysubfolders[_Idx]);
+	}
+
+	/**
+	 * \return size of current directory
+	 */
+	MATRICE_HOST_INL constexpr auto size() const {
+		return (_Mysubfolders.size());
 	}
 
 private:
-	path_type _Mypath;
-};
-}
+	MATRICE_HOST_INL void _Ext_subfolder() {
+		_Mysubfolders.clear();
+		auto _Path = _Mypath.string();
+		fs::directory_iterator _End;
+		for (decltype(_End) _Begin(_Mypath); _Begin != _End; ++_Begin) {
+			if (fs::is_directory(_Begin->status())) {
+				auto _Sub = _Begin->path().string().substr(_Path.size());
+				_Mysubfolders.emplace_back(_Sub);
+			}
+		}
+		_Mysubfolders.shrink_to_fit();
+	}
 
+	path_type _Mypath;
+	container _Mysubfolders;
+};
+_DETAIL_END
+
+using directory = detail::_Dir_impl<detail::folder_tag>;
 
 } DGE_MATRICE_END
