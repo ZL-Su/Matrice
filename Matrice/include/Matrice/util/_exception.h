055c9d030acb21ca828f28ac3fab46cc694d1c05
Zhilong Su <su-zl@seu.edu.cn>
2019-01-06 14:58:00 +0800

    Add type dgelom::detail::_Source_location to save target code location.

--- Matrice/include/Matrice/util/_exception.h
+++ Matrice/include/Matrice/util/_exception.h
@@ -5,5 +5,14 @@
 #include <exception>
 #include "_macros.h"
 
-DGE_MATRICE_BEGIN
-DGE_MATRICE_END
+DGE_MATRICE_BEGIN _DETAIL_BEGIN
+/**
+ * \record source code location
+ * \Example: _Source_location _Loc{__func__, __FILE__, __LINE__}
+ */
+struct _Source_location {
+	const char* _Func = nullptr;
+	const char* _File = nullptr;
+	long        _Line;
+};
+_DETAIL_END DGE_MATRICE_END
