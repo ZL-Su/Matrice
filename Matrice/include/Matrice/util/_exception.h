76222556e13ec54b63814bd5cfd9b9bcf5643f1c
Zhilong Su <su-zl@seu.edu.cn>
2019-01-06 14:49:25 +0800

    Create _exception.h

---
+++ Matrice/include/Matrice/util/_exception.h
@@ -0,0 +1,9 @@
+/*********************************************************************
+	  About License Agreement of this file, see "../lic/license.h"
+*********************************************************************/
+#pragma once
+#include <exception>
+#include "_macros.h"
+
+DGE_MATRICE_BEGIN
+DGE_MATRICE_END
