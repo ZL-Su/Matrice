241353b86855c6a39a7725018a3e505d7d03a35c
Zhilong Su <su-zl@seu.edu.cn>
2019-01-10 13:32:49 +0800

    Add _std_wrapper.h

---
+++ Matrice/include/Matrice/util/_std_wrapper.h
@@ -0,0 +1,16 @@
+/*********************************************************************
+	  About License Agreement of this file, see "../lic/license.h"
+*********************************************************************/
+#pragma once
+#include <cstdint>
+#include <queue>
+
+namespace dgelom {
+	using std::uint8_t;
+	using std::uint16_t;
+	using std::uint32_t;
+	using std::uint64_t;
+	using std::size_t;
+	template<typename... _Ts>
+	using priority_queue = std::priority_queue<_Ts...>;
+}
