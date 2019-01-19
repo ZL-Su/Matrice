5fdd0bd2fd72c661b06057e4c586f6915da7c18e
Zhilong Su <su-zl@seu.edu.cn>
2019-01-19 15:37:34 +0800

    Add static test on supported system.

--- Matrice/include/Matrice/util/utils.h
+++ Matrice/include/Matrice/util/utils.h
@@ -10,6 +10,8 @@
 #endif
 DGE_MATRICE_BEGIN
 
+static_assert(sizeof(void *) == 8, "MATRICE supports 64 bit only");
+
 template<typename _Ty = long double>
 MATRICE_GLOBAL_INL constexpr _Ty ¦Ð = static_cast<_Ty>(3.1415926535897932384626433832795028841971693993751058209749445923078164062862);
 
