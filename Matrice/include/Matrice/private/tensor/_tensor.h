4f89e1726c8d4b49ba6c86245021d6a29b213965
Zhilong Su <su-zl@seu.edu.cn>
2019-01-21 16:02:38 +0800

    Remove _Tensor_shape, shape3, shape4 and update dgelom::tensor<_Ty>.

--- Matrice/include/Matrice/private/tensor/_tensor.h
+++ Matrice/include/Matrice/private/tensor/_tensor.h
@@ -8,62 +8,6 @@ DGE_MATRICE_BEGIN
 _DETAIL_BEGIN
 namespace common = types;
 
-using shape3 = tuple<size_t, size_t, size_t>;
-using shape4 = tuple<size_t, size_t, size_t, size_t>;
-
-namespace tensor_impl {
-struct _Tensor_shape {
-#define _TSHAPE_BATCHS std::get<0>(data)
-#define _TSHAPE_CHANNELS std::get<0>(std::get<1>(data))
-#define _TSHAPE_SHAPE(I) std::get<I>(std::get<1>(std::get<1>(data)))
-	using _Myt = _Tensor_shape;
-	using type = tuple<size_t, tuple<size_t, shape>>;
-	MATRICE_HOST_INL _Tensor_shape(const type& _Shape)
-		:data(_Shape) {}
-	MATRICE_HOST_INL _Tensor_shape(type&& _Shape)
-		: data(std::move(_Shape)) {}
-	MATRICE_HOST_INL _Tensor_shape(const shape & _Shape)
-		:data{1,{1,_Shape}} {}
-	MATRICE_HOST_INL _Tensor_shape(const shape3& _Shape)
-		:data{1,{std::get<0>(_Shape), {std::get<1>(_Shape),std::get<2>(_Shape)}}} {}
-	MATRICE_HOST_INL _Tensor_shape(const shape4& _Shape)
-		:data{ std::get<0>(_Shape), {std::get<1>(_Shape), {std::get<2>(_Shape),std::get<3>(_Shape)}}}{}
-	template<typename _Ity>
-	MATRICE_HOST_INL _Tensor_shape(std::initializer_list<_Ity> _Shape){
-		if (_Shape.size() == 2) {
-			data = {1,{1,{(size_t)*_Shape.begin(), (size_t)*(_Shape.begin()+1)}}};
-		}
-		if (_Shape.size() == 3) {
-			data = { 1,{(size_t)*_Shape.begin(),{(size_t)*(_Shape.begin() + 1), (size_t)*(_Shape.begin() + 2)}} };
-		}
-		if (_Shape.size() == 4) {
-			data = { (size_t)*_Shape.begin(),{(size_t)*(_Shape.begin() + 1),{(size_t)*(_Shape.begin() + 2), (size_t)*(_Shape.begin() + 3)}} };
-		}
-	}
-	MATRICE_HOST_INL auto& operator= (const _Myt& _Oth) {
-		data = (_Oth.data); return (*this);
-	}
-	MATRICE_HOST_INL auto& operator= (_Myt&& _Oth) {
-		data = std::move(_Oth.data); return (*this);
-	}
-	MATRICE_HOST_INL constexpr auto operator()() const {
-		return shape4{_TSHAPE_BATCHS, _TSHAPE_CHANNELS,
-			_TSHAPE_SHAPE(0), _TSHAPE_SHAPE(1) };
-	}
-	MATRICE_HOST_INL constexpr auto rows() const {
-		return _TSHAPE_BATCHS * _TSHAPE_SHAPE(0);
-	}
-	MATRICE_HOST_INL constexpr auto cols() const {
-		return _TSHAPE_CHANNELS * _TSHAPE_SHAPE(1);
-	}
-
-	type data;
-#undef _TSHAPE_BATCHS
-#undef _TSHAPE_CHANNELS
-#undef _TSHAPE_SHAPE
-};
-}
-
 template<typename _Ty>
 class _Tensor : public common::Base_<_Tensor<_Ty>>
 {
@@ -74,7 +18,7 @@ public:
 	using typename _Mybase::value_t;
 	using typename _Mybase::value_type;
 	using typename _Mybase::const_init_list;
-	using tensor_shape = tensor_impl::_Tensor_shape;
+	using tensor_shape = basic_shape<size_t>;
 
 	/**
 	 *\brief Empty constructor
@@ -84,44 +28,44 @@ public:
 	/**
 	 *\brief Construct a tensor with shape [1,[1,_Shape]]
 	 */
-	_Tensor(const shape& _Shape)
-		: _Mybase(_Shape), _Myshape(_Shape) {}
+	_Tensor(shape_t<size_t>&& _Shape)
+		: _Mybase(_Shape), _Myshape(std::move(_Shape)) {}
 	/**
 	 *\brief Construct a tensor with shape [1,[1,_Shape]] and fill with _Val
 	 */
-	_Tensor(const shape& _Shape, value_t _Val)
-		: _Mybase(_Shape, _Val), _Myshape(_Shape) {}
+	_Tensor(shape_t<size_t>&& _Shape, value_t _Val)
+		: _Mybase(_Shape, _Val), _Myshape(std::move(_Shape)) {}
 	/**
 	 *\brief Construct a tensor with shape [1,[_Shape]]
 	 */
-	_Tensor(const shape3& _Shape)
-		: _Mybase(std::get<2>(_Shape), std::get<1>(_Shape)*std::get<0>(_Shape)), _Myshape(_Shape) {}
+	_Tensor(shape3_t<size_t>&& _Shape)
+		: _Mybase(std::get<2>(_Shape), std::get<1>(_Shape)*std::get<0>(_Shape)), _Myshape(std::move(_Shape)) {}
 	/**
 	 *\brief Construct a tensor with shape [1,[_Shape]] and fill with _Val
 	 */
-	_Tensor(const shape3& _Shape, value_t _Val)
-		: _Tensor(_Shape) { _Mybase::operator= ({ _Val }); }
+	_Tensor(shape3_t<size_t>&& _Shape, value_t _Val)
+		: _Tensor(std::move(_Shape)) { _Mybase::operator= ({ _Val }); }
 	/**
 	 *\brief Construct a tensor with shape _Shape
 	 */
-	_Tensor(const shape4& _Shape) 
-		: _Mybase(std::get<0>(_Shape)*std::get<2>(_Shape), std::get<1>(_Shape)*std::get<3>(_Shape)), _Myshape(_Shape) {}
+	_Tensor(shape4_t<size_t>&& _Shape) 
+		: _Mybase(std::get<0>(_Shape)*std::get<2>(_Shape), std::get<1>(_Shape)*std::get<3>(_Shape)), _Myshape(std::move(_Shape)) {}
 	/**
 	 *\brief Construct a tensor with shape _Shape and fill with _Val
 	 */
-	_Tensor(const shape4& _Shape, value_t _Val)
-		: _Tensor(_Shape) {_Mybase::operator= ({_Val });}
+	_Tensor(shape4_t<size_t>&& _Shape, value_t _Val)
+		: _Tensor(std::move(_Shape)) {_Mybase::operator= ({_Val });}
 	/**
 	 *\brief Move constructor
 	 */
 	_Tensor(_Myt&& _Other) noexcept
-		: _Mybase(std::move(_Other)), _Myshape(_Other.shape().data) {}
+		: _Mybase(std::move(_Other)), _Myshape(std::move(_Other.shape()())) {}
 
 	/**
 	 *\brief Create a tensor
 	 *\param [_Shape] any shape type compatible with tensor_shape 
 	 */
-	MATRICE_HOST_INL auto& create(const tensor_shape& _Shape) {
+	MATRICE_HOST_INL auto& create(tensor_shape&& _Shape) {
 		_Myshape = std::move(_Shape);
 		create(_Myshape.rows(), _Myshape.cols());
 		return (*this);
@@ -140,9 +84,9 @@ public:
 	 *\brief Generate a random filled tensor
 	 *\param [_Shape] any shape type compatible with tensor_shape
 	 */
-	static MATRICE_HOST_INL auto rand(const tensor_shape& _Shape) {
+	static MATRICE_HOST_INL auto rand(tensor_shape&& _Shape) {
 		auto _Ret = _Mybase::rand(_Shape.rows(), _Shape.cols());
-		_Ret.shape() = std::move(_Shape);
+		_Ret._Myshape = std::move(_Shape);
 		return (std::move(_Ret));
 	}
 
