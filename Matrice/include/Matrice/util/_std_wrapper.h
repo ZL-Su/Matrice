06c546d5622c12742b0dd8f8822467f6acdebf82
Zhilong Su <su-zl@seu.edu.cn>
2019-01-21 16:01:10 +0800

    Update to let Matrice compatible with STD version before C++ 14.

--- Matrice/include/Matrice/util/_std_wrapper.h
+++ Matrice/include/Matrice/util/_std_wrapper.h
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <queue>
 #include <random>
+#include <type_traits>
 
 namespace dgelom {
 	using std::uint8_t;
@@ -21,4 +22,28 @@ namespace dgelom {
 	using std::uniform_real;
 
 	using diff_t = std::ptrdiff_t;
+
+#if defined(_HAS_CXX14) || defined(_HAS_CXX17)
+	using std::enable_if_t;
+	using std::common_type_t;
+	using std::is_integral_v;
+	using std::is_arithmetic_v;
+	using std::is_scalar_v;
+#else
+#define MATRICE_MAKE_TS_HELPER(TYPE) \
+	template<typename _Ty> \
+	inline constexpr auto is_##TYPE##_v \
+	= std::is_##TYPE##<_Ty>::value;
+
+	template<bool _Test, typename _Ty = void>
+	using enable_if_t = typename std::enable_if<_Test, _Ty>::type;
+	template<typename... _Ty>
+	using common_type_t = typename std::common_type<_Ty...>::type;
+	
+	MATRICE_MAKE_TS_HELPER(integral)
+	MATRICE_MAKE_TS_HELPER(arithmetic)
+	MATRICE_MAKE_TS_HELPER(scalar)
+
+#undef MATRICE_MAKE_TS_HELPER
+#endif
 }
