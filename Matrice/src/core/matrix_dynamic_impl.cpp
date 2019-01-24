15476e9e4238334d16b80e080c939d42791e4429
Zhilong Su <su-zl@seu.edu.cn>
2019-01-24 18:54:29 +0800

    Add __create_impl(rows, cols) implementations for uniform matrix and device matrix.

--- Matrice/src/core/matrix_dynamic_impl.cpp
+++ Matrice/src/core/matrix_dynamic_impl.cpp
@@ -3,15 +3,35 @@
 MATRICE_NAMESPACE_BEGIN_TYPES
 
 #define MATRICE_INSTANTIATE_METHOD_CREATE(TYPE) \
-template void Matrix_<TYPE, 0,0>::__create_impl(diff_t, diff_t);
+template void Matrix_<TYPE, 0, 0>::__create_impl(size_t, size_t);\
+template void Matrix_<TYPE,-1, 0>::__create_impl(size_t, size_t);\
+template void Matrix_<TYPE,-1,-1>::__create_impl(size_t, size_t);
 
 template<typename _Ty>
-void Matrix_<_Ty, 0, 0>::__create_impl(diff_t rows, diff_t cols) {
-	_Mybase::m_storage = std::move(detail::Storage_<value_t>::Allocator<0, 0>(rows, cols));
+void Matrix_<_Ty, 0, 0>::__create_impl(size_t rows, size_t cols) {
+	_Mybase::m_storage = std::move(decltype(_Mybase::m_storage)(rows, cols));
 	_Mybase::m_rows = rows, _Mybase::m_cols = cols;
+	_Mybase::_Myshape = { 1,1,rows,cols };
 	_Mybase::m_data = internal::_Proxy_checked(_Mybase::m_storage.data());
 	_Mybase::base_t::_Flush_view_buf();
 }
+template<typename _Ty>
+void Matrix_<_Ty, -1, 0>::__create_impl(size_t rows, size_t cols) {
+	_Mybase::m_storage = decltype(_Mybase::m_storage)(rows, cols);
+	_Mybase::m_rows = rows, _Mybase::m_cols = cols;
+	_Mybase::_Myshape = { 1,1,rows,cols };
+	_Mybase::m_data = internal::_Proxy_checked(_Mybase::m_storage.data());
+	_Mybase::base_t::_Flush_view_buf();
+}
+template<typename _Ty>
+void Matrix_<_Ty, -1, -1>::__create_impl(size_t rows, size_t cols) {
+	_Mybase::m_storage = decltype(_Mybase::m_storage)(rows, cols);
+	_Mybase::m_rows = rows, _Mybase::m_cols = cols;
+	_Mybase::_Myshape = { 1,1,rows,cols };
+	_Mybase::m_data = internal::_Proxy_checked(_Mybase::m_storage.data());
+	_Mybase::base_t::_Flush_view_buf();
+}
+
 MATRICE_INSTANTIATE_METHOD_CREATE(int)
 MATRICE_INSTANTIATE_METHOD_CREATE(bool)
 MATRICE_INSTANTIATE_METHOD_CREATE(char)
