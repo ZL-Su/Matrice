93d269b1c6ee31a88e7440d5e44de966a94af27a
Zhilong Su <su-zl@seu.edu.cn>
2019-02-24 15:33:43 +0800

    Update license region.

--- Matrice/include/Matrice/util/genalgs.h
+++ Matrice/include/Matrice/util/genalgs.h
@@ -1,20 +1,6 @@
-/***************************************************************************
-This file is part of Matrice, an effcient and elegant C++ library for SC.
-Copyright(C) 2018, Zhilong (Dgelom) Su, all rights reserved.
-
-This program is free software : you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option)
-any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+/*********************************************************************
+	  About License Agreement of this file, see "../lic/license.h"
+*********************************************************************/
 #pragma once
 #include <xutility>
 #include<algorithm>
@@ -140,8 +126,7 @@ MATRICE_GLOBAL_INL auto reduce(_InIt _First, _InIt _Last) {
 }
 
 // \return: sum of range [_First, _Last) with step := _Stride
-template<typename _InIt, 
-	typename = std::enable_if_t<std::is_pointer_v<_InIt>>>
+template<typename _InIt, MATRICE_ENABLE_IF(is_pointer_v<_InIt>)>
 MATRICE_GLOBAL_INL auto reduce(_InIt _First, _InIt _Last, index_t _Stride) {
 	static_cast<void>(_First == _Last);
 	remove_reference_t<decltype(*_First)> _Ret = 0;
@@ -156,7 +141,7 @@ MATRICE_GLOBAL_INL _Ty reduce(_InIt _First, _InIt _Last, _Op _op) {
 	for (; _First != _Last; ++_First) _Ret += _op(*_First);
 	return (_Ret);
 }
-template<typename _InIt, typename _Op, typename = std::enable_if_t<std::is_pointer_v<_InIt>&&std::is_function_v<_Op>>>
+template<typename _InIt, typename _Op, MATRICE_ENABLE_IF(is_pointer_v<_InIt>&&is_function_v<_Op>)>
 MATRICE_GLOBAL_INL auto reduce(_InIt _First, _InIt _Last, _Op _op) {
 	static_cast<void>(_First == _Last);
 	remove_reference_t<decltype(_First[0])> _Ret = 0;
@@ -166,7 +151,7 @@ MATRICE_GLOBAL_INL auto reduce(_InIt _First, _InIt _Last, _Op _op) {
 template<template<typename> class _Op, 
 	typename _InIt, typename _Scalar, typename _Func>
 MATRICE_GLOBAL_INL auto reduce(_InIt _First, _InIt _Last, _Scalar _Value, _Func _Fn, _Op<_Scalar> _op = _Op<_Scalar>()) {
-	static_assert(std::is_arithmetic_v<_Scalar>, "Oops, template parameter '_Scalar' is illegal!");
+	static_assert(is_arithmetic_v<_Scalar>, "Oops, template parameter '_Scalar' is illegal!");
 	static_cast<void>(_First == _Last);
 	remove_reference_t<decltype(_First[0])> _Ret = 0;
 	for (; _First != _Last; ++_First) _Ret += _Fn(_op(*_First, _Value));
