5a01b6698c650f8d0ae325d09e47e9d1adcb2756
Zhilong Su <su-zl@seu.edu.cn>
2019-03-30 15:54:26 +0800

    Update and add ReLU activation function.

--- Matrice/include/Matrice/algs/dnn/functions/_functions.h
+++ Matrice/include/Matrice/algs/dnn/functions/_functions.h
@@ -29,25 +29,24 @@ struct sigmoid {
 	 *\brief for executing nodal activation(s):
 	  //tex:
 	  //$$\sigma(x) = \dfrac{1}{1+e^{-x}}$$
-	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor
+	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor.
 	 */
 	template<typename _Ty> 
 	MATRICE_GLOBAL_INL static _Ty forward(const _Ty& x) {
 		using value_t = conditional_t<is_scalar_v<_Ty>, _Ty, 
 			typename _Ty::value_t>;
-		return (one<value_t>/(one<value_t> + exp(value_t(-1)*x)));
+		return (one<value_t>/(one<value_t> + exp(-1*x)));
 	}
 	/**
 	 *\brief for evaluating gradient(s) w.r.t. the input:
 	  //tex:
 	  //$$\sigma'(x) = \sigma(x)(1-\sigma(x))$$
-	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor
+	 *\param [y] data produced by sigmoid::forward().
 	 */
 	template<typename _Ty>
-	MATRICE_GLOBAL_INL static _Ty backward(const _Ty& x) {
+	MATRICE_GLOBAL_INL static _Ty backward(const _Ty& y) {
 		using value_t = conditional_t<is_scalar_v<_Ty>, _Ty, 
 			typename _Ty::value_t>;
-		const auto y = forward(x);
 		return (y * (one<value_t> - y));
 	}
 };
@@ -58,29 +57,60 @@ struct tanh {
 	 *\brief for executing nodal activation(s):
 	  //tex:
 	  //$$\sigma(x) = \dfrac{2}{1+e^{-2x}}-1$$
-	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor
+	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor.
 	 */
 	template<typename _Ty>
 	MATRICE_GLOBAL_INL static _Ty forward(const _Ty& x) {
-		using value_t = conditional_t<is_scalar_v<_Ty>, _Ty,
-			typename _Ty::value_t>;
-		return two<value_t>/(one<value_t> + exp(value_t(-2)*x))-1;
+		using value_t = conditional_t<is_scalar_v<_Ty>, 
+			_Ty, typename _Ty::value_t>;
+		return two<value_t>/(one<value_t> + exp(-2*x)) - one<value_t>;
 	}
 	/**
 	 *\brief evaluating gradient(s) w.r.t. the input:
 	  //tex:
 	  //$$\sigma'(x) = 1-\sigma^2(x)$$
-	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor
+	 *\param [y] data produced by tanh::forward().
 	 */
 	template<typename _Ty>
-	MATRICE_GLOBAL_INL static _Ty backward(const _Ty& x) {
-		using value_t = conditional_t<is_scalar_v<_Ty>, _Ty,
-			typename _Ty::value_t>;
-		const auto y = forward(x);
+	MATRICE_GLOBAL_INL static _Ty backward(const _Ty& y) {
+		using value_t = conditional_t<is_scalar_v<_Ty>, 
+			_Ty, typename _Ty::value_t>;
 		return (one<value_t> - sqr(y));
 	}
 };
 
+/* Rectified Linear Unit activation function */
+struct relu {
+	/**
+	 *\brief for executing nodal activation(s):
+	  //tex:
+	  //$$\sigma(x) = \max(0,x)$$
+	 *\param [x] input data with a type of scalar, ::Matrix or ::tensor.
+	 */
+	template<typename _Ty>
+	MATRICE_GLOBAL_INL static _Ty forward(const _Ty& x) {
+		using value_t = conditional_t<is_scalar_v<_Ty>,
+			_Ty, typename _Ty::value_t>;
+		return max(zero<value_t>, x);
+	}
+	/**
+	 *\brief evaluating gradient(s) w.r.t. the input:
+	  //tex:
+	  //$$\sigma'(x) = \left\{
+     //\begin{array}{lr}1,&x>0\\0,&x\leq0\end{array}\right.$$
+	 *\param [y] data produced by relu::forward().
+	 */
+	template<typename _Ty>
+	MATRICE_GLOBAL_INL static _Ty backward(const _Ty& y) {
+		using value_t = conditional_t<is_scalar_v<_Ty>,
+			_Ty, typename _Ty::value_t>;
+		if constexpr (is_scalar_v<_Ty>) return y > 0 ? 1 : 0;
+		else {
+			_Ty _Ret(y.shape());
+			return std::forward<_Ty>(_Ret.from(y.data(), [](auto _val) { return backward(_val); }));
+		}
+	}
+};
 
 };
 }
