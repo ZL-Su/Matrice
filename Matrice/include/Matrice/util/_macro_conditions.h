38fadaf7f575d07817157fa8d2606a0e801b394c
Zhilong Su <su-zl@seu.edu.cn>
2019-04-10 19:05:50 +0800

    Update _macro_conditions.h

--- Matrice/include/Matrice/util/_macro_conditions.h
+++ Matrice/include/Matrice/util/_macro_conditions.h
@@ -17,6 +17,8 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 #pragma once
 
+#pragma warning (disable: 4067)
+
 #include "_macros.h"
 
 DGE_MATRICE_BEGIN
