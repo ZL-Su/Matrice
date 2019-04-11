b82b60e1281851c1da93b06cb39bc8f0fd9d8ded
Zhilong Su <su-zl@seu.edu.cn>
2019-04-11 11:56:26 +0800

    Add mkl support to matrix determinant :basketball:

--- Matrice/src/core/matrix_base_impl.cpp
+++ Matrice/src/core/matrix_base_impl.cpp
@@ -1,6 +1,6 @@
 /**********************************************************************
     This file is part of Matrice, an effcient and elegant C++ library.
-        Copyright(C) 2018, Zhilong(Dgelom) Su, all rights reserved.
+     Copyright(C) 2018-2019, Zhilong(Dgelom) Su, all rights reserved.
 
 This program is free software :  you can redistribute it and/or modify
 it  under the terms of the  GNU General Public License as published by
@@ -16,27 +16,29 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 ***********************************************************************/
 #include "../../include/Matrice/core/matrix.h"
-//#ifdef __use_mkl__
-//#include <mkl.h>
-//#else
-#include <fkl.h>
-//#endif
 
 MATRICE_NAMESPACE_BEGIN_
 
-template<typename _Rhs, typename value_t> value_t det_impl(const _Rhs & a)
-{
-	const int N = a.rows();
-	const auto p = a.data();
+template<typename _Rhs, typename value_t> 
+value_t det_impl(const _Rhs & a) {
+	const int N = a.rows(); const auto p = a.data();
+	if (N == 1) return (p[0]);
 	if (N == 2) return (p[0]*p[3] - p[1]*p[2]);
 	if (N == 3) return (p[0]*(p[4]*p[8] - p[5]*p[7]) - p[1]*(p[3]*p[8] - p[5]*p[6]) + p[2]*(p[3]*p[7] - p[4]*p[6]));
 
+#if MATRICE_MATH_KERNEL==MATRICE_USE_MKL
+	lapack_kernel<value_t>::lud(a);
+	auto _Ret = one<value_t>;
+	for (int i = 0; i < N; ++i) _Ret *= a[i][i];
+	return (_Ret);
+#elif MATRICE_MATH_KERNEL==MATRICE_USE_FKL
 	if constexpr (type_bytes<value_t>::value == 4)
 		return fblas::_sdetm(fkl::sptr(p), a.rows());
 	if constexpr (type_bytes<value_t>::value == 8)
 		return fblas::_ddetm(fkl::dptr(p), a.rows());
-	
+#else
 	return std::numeric_limits<value_t>::infinity();
+#endif
 }
 template float det_impl(const types::Matrix_<float, 2, 2>&);
 template float det_impl(const types::Matrix_<float, 3, 3>&);
