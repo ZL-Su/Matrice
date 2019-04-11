503806f84a2b6ea75efd8949077c16d19b666faa
Zhilong Su <su-zl@seu.edu.cn>
2019-04-11 11:54:22 +0800

    Remove det() function.

--- Matrice/include/Matrice/funcs/_functions.h
+++ Matrice/include/Matrice/funcs/_functions.h
@@ -27,57 +27,30 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #include "../private/math/_config.h"
 
 DGE_MATRICE_BEGIN
-_DETAIL_BEGIN
-template<typename _Ty> MATRICE_HOST_INL constexpr
-_Ty _Det(const _Ty* data, int n, enable_if_t<is_float32_v<_Ty>>* = 0) {
-	return fblas::_sdetm(static_cast<float*>(data), n);
-}
-template<typename _Ty> MATRICE_HOST_INL constexpr
-_Ty _Det(const _Ty* data, int n, enable_if_t<is_float64_v<_Ty>>* = 0) {
-	return fblas::_ddetm(static_cast<double*>(data), n);
-}
-_DETAIL_END
-
-/**
- * \SIMD supported determinant of a matrix or its derivatives.
- */
-template<typename _T, MATRICE_ENABLE_IF(is_matrix_convertible_v<_T>)>
-MATRICE_HOST_INL constexpr typename _T::value_t det(const _T& _x) {
-	return detail::_Det(_x.eval().data(), _x.rows());
-}
-
 /**
  * \SIMD supported dot-product of two vectors.
  */
 template<typename _T, typename _U, 
-	typename _Vty = common_type_t<typename _T::value_t, typename _U::value_t>>
-MATRICE_HOST_INL constexpr _Vty dot(const _T& _x, const _U& _y) {
+	typename value_t = common_type_t<typename _T::value_t, typename _U::value_t>>
+MATRICE_HOST_INL constexpr value_t dot(const _T& _x, const _U& _y) {
 #ifdef _DEBUG
 	DGELOM_CHECK(_x.size()== _y.size(), "Oops, non-consistent size error.");
 #endif
-
-	auto _Ret = zero<_Vty>;
-
-#if   MATRICE_MATH_KERNEL == MATRICE_USE_MKL
-
-#elif MATRICE_MATH_KERNEL == MATRICE_USE_FKL
-
-#else //MATRICE_MATH_KERNEL == MATRICE_USE_NAT
-	using packet_type = simd::Packet_<_Vty>;
+	using packet_type = simd::Packet_<value_t>;
 
 	const auto _Left = _x.eval();
 	const auto _Right = _y.eval();
 	const auto _Size = min(_Left.size(), _Right.size());
 	const auto _Vsize = simd::vsize<packet_type::size>(_Size);
+
+	auto _Ret = zero<value_t>;
 	for (auto _Idx = 0; _Idx < _Vsize; _Idx += packet_type::size) {
-		_Ret += (
-			packet_type(_Left.data() + _Idx)*
+		_Ret += (packet_type(_Left.data() + _Idx)*
 			packet_type(_Right.data() + _Idx)).reduce();
 	}
 	for (auto _Idx = _Vsize; _Idx < _Size; ++_Idx) {
 		_Ret += _Left(_Idx)*_Right(_Idx);
 	}
-#endif
 
 	return (_Ret);
 }
