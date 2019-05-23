508372296daf871bfa94637e977afa077948694a
Zhilong Su <su-zl@seu.edu.cn>
2019-05-23 14:14:54 +0800

    Optimize range base type.

--- Matrice/include/Matrice/private/_range.h
+++ Matrice/include/Matrice/private/_range.h
@@ -9,6 +9,7 @@ template<typename _Ty,
 	is_pointer_v<_Ty>||std::_Is_iterator_v<_Ty>, 
 	int64_t, _Ty>>
 class _Range_base {
+	using _Myt = _Range_base;
 public:
 	using value_t = _Ty;
 	using step_t = _Iy;
@@ -17,23 +18,24 @@ public:
 
 	class _My_iterator {
 	public:
-		MATRICE_GLOBAL_INL _My_iterator(const_value_t& _Pos, const_step_t&_Inc = 1) : _My_pos(_Pos), _My_inc(_Inc) {}
+		MATRICE_GLOBAL_INL _My_iterator(const_value_t& _Pos, const_step_t&_Inc = 1) noexcept
+			: _My_pos(_Pos), _My_inc(_Inc) {}
 
-		MATRICE_GLOBAL_INL value_t& operator++() { 
+		MATRICE_GLOBAL_INL value_t& operator++() noexcept {
 			return _My_pos += _My_inc; 
 		}
-		MATRICE_GLOBAL_INL const value_t& operator++() const {
+		MATRICE_GLOBAL_INL const value_t& operator++() const noexcept {
 			return _My_pos += _My_inc; 
 		}
 
-		MATRICE_GLOBAL_INL value_t& operator*() { return _My_pos; }
-		MATRICE_GLOBAL_INL const value_t& operator*() const { return _My_pos; }
+		MATRICE_GLOBAL_INL value_t& operator*() noexcept { return _My_pos; }
+		MATRICE_GLOBAL_INL const value_t& operator*() const noexcept { return _My_pos; }
 
-		MATRICE_GLOBAL_INL auto operator != (const _My_iterator& _Other)const {
+		MATRICE_GLOBAL_INL auto operator != (const _My_iterator& _Other)const noexcept {
 			return _My_pos < (_Other._My_pos);
 		}
 
-		MATRICE_GLOBAL_INL operator value_t() const { return _My_pos; }
+		MATRICE_GLOBAL_INL operator value_t() const noexcept { return _My_pos; }
 
 	private:
 		value_t _My_pos;
@@ -45,39 +47,47 @@ public:
 	_Range_base(const _Range_base& _other) noexcept
 		:m_begin(_other.m_begin), m_end(_other.m_end), m_stride(_other.m_stride) {}
 
-	MATRICE_GLOBAL_INL _Range_base& operator= (const _Range_base& _other) {
+	MATRICE_GLOBAL_INL _Range_base& operator= (const _Range_base& _other) noexcept {
 		m_begin = _other.m_begin, m_end = _other.m_end;
 		m_stride = _other.m_stride;
 		return (*this);
 	}
-	MATRICE_GLOBAL_INL _Range_base& operator= (const std::initializer_list<value_t> _L) {
+	MATRICE_GLOBAL_INL _Range_base& operator= (const initlist<value_t> _L) noexcept {
 		m_begin = *_L.begin(), m_end = *(_L.begin()+1);
 		if (_L.size() == 2) m_stride = value_t(1);
 		if (_L.size() == 3) m_stride = *(_L.begin() + 2);
 		return (*this);
 	}
 
-	MATRICE_GLOBAL_INL value_t operator[](index_t i) { 
+	MATRICE_GLOBAL_INL value_t operator[](index_t i) noexcept {
 		return (m_pos = m_begin + i * m_stride); 
 	}
-	MATRICE_GLOBAL_INL const_value_t operator[](index_t i) const { 
+	MATRICE_GLOBAL_INL const_value_t operator[](index_t i) const noexcept {
 		return (m_pos = m_begin + i * m_stride); 
 	}
-	MATRICE_GLOBAL_INL value_t operator()(index_t i) { 
+	MATRICE_GLOBAL_INL value_t operator()(index_t i) noexcept {
 		return operator[](i); 
 	}
-	MATRICE_GLOBAL_INL const_value_t operator()(index_t i) const { 
+	MATRICE_GLOBAL_INL const_value_t operator()(index_t i) const noexcept {
 		return operator[](i); 
 	}
 
-	MATRICE_GLOBAL_INL value_t& value() { return m_pos; }
-	MATRICE_GLOBAL_INL const_value_t& value() const { return m_pos; }
-	MATRICE_GLOBAL_INL size_t size() const { 
+	MATRICE_GLOBAL_INL value_t& value() noexcept { 
+		return m_pos; 
+	}
+	MATRICE_GLOBAL_INL const_value_t& value() const noexcept { 
+		return m_pos; 
+	}
+	MATRICE_GLOBAL_INL size_t size() const noexcept { 
 		return (m_end - m_begin) / m_stride; 
 	}
 
-	MATRICE_GLOBAL_INL value_t& front() { return m_begin; }
-	MATRICE_GLOBAL_INL const value_t& front() const { return m_begin; }
+	MATRICE_GLOBAL_INL value_t& front() noexcept {
+		return m_begin; 
+	}
+	MATRICE_GLOBAL_INL const value_t& front() const noexcept {
+		return m_begin; 
+	}
 
 	MATRICE_GLOBAL_INL _My_iterator begin() { 
 		return _My_iterator(m_pos, m_stride); 
@@ -92,7 +102,28 @@ public:
 		return _My_iterator(m_end, zero<step_t>);
 	}
 
-	MATRICE_GLOBAL_INL operator bool() const { 
+	/**
+	 *\brief set the starting position.
+	 */
+	MATRICE_GLOBAL_INL _Myt& begin(value_t _Beg) noexcept {
+		m_begin = _Beg; return (*this);
+	}
+	/**
+	 *\brief set the last position.
+	 */
+	MATRICE_GLOBAL_INL _Myt& end(value_t _End) noexcept {
+		m_end = _End; return (*this);
+	}
+	/**
+	 *\brief set the stride of a range.
+	 */
+	MATRICE_GLOBAL_INL _Myt& stride(step_t _Std) noexcept {
+		m_stride = _Std; return (*this);
+	}
+	/**
+	 *\brief check if the pos of the range meets the end. 
+	 */
+	MATRICE_GLOBAL_INL operator bool() const noexcept {
 		return (m_pos + m_stride < m_end); 
 	}
 
@@ -139,13 +170,13 @@ class range MATRICE_NONHERITABLE
 	: public detail::_Range_base<common_type_t<_Ty, _Uy>> {
 	using _Mybase = detail::_Range_base<common_type_t<_Ty, _Uy>>;
 public:
-	MATRICE_GLOBAL_INL range(const _Ty& _First, const _Uy& _Last)
+	explicit MATRICE_GLOBAL_INL range(const _Ty& _First, const _Uy& _Last)
 		: _Mybase(_First, _Last) {}
-	MATRICE_GLOBAL_INL range(const _Ty& _First, const _Uy& _Last, typename _Mybase::const_step_t& _Inc)
+	explicit MATRICE_GLOBAL_INL range(const _Ty& _First, const _Uy& _Last, typename _Mybase::const_step_t& _Inc)
 		: _Mybase(_First, _Last, _Inc) {}
-	MATRICE_GLOBAL_INL range(const _Ty& _First, const size_t& _Size)
+	explicit MATRICE_GLOBAL_INL range(const _Ty& _First, size_t _Size)
 		: _Mybase(_First, _First+_Size) {}
-	MATRICE_GLOBAL_INL range(const _Ty& _First, const size_t& _Size, typename _Mybase::const_step_t& _Inc)
+	explicit MATRICE_GLOBAL_INL range(const _Ty& _First, size_t _Size, typename _Mybase::const_step_t& _Inc)
 		: _Mybase(_First, _First+_Size, _Inc) {}
 };
 
