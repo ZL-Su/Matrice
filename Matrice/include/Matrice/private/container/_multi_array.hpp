71fb03389875998899c60df9c1371b3354e714f9
Zhilong Su <su-zl@seu.edu.cn>
2019-05-23 15:57:11 +0800

    Update _multi_array.hpp

--- Matrice/include/Matrice/private/container/_multi_array.hpp
+++ Matrice/include/Matrice/private/container/_multi_array.hpp
@@ -22,20 +22,21 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 DGE_MATRICE_BEGIN
 
 /**
- *\brief description
+ *\brief CLASS TEMPLATE multi-dimensional array: multi_array
  *\param <_N> dimensions of multi_array object.
  */
 template<typename _Ty, size_t _N> class multi_array { 
 	static_assert(is_scalar_v<_Ty>, "_Ty must be a scalar type.");
 
 	using _Myt = multi_array;
-	using _Mybuf = Matrix_<_Ty, 0, 0>;
+	using _Mybuf_t = Matrix_<_Ty, 0, 0>;
 public:
 	static constexpr auto dimension = _N;
-	using pointer = typename _Mybuf::pointer;
-	using value_type = typename _Mybuf::value_type;
+	using pointer = typename _Mybuf_t::pointer;
+	using value_type = typename _Mybuf_t::value_type;
 	using index_type = array_n<size_t, dimension>;
 	using reference = std::add_lvalue_reference_t<value_type>;
+	using difference_type = std::ptrdiff_t;
 
 	multi_array() noexcept {
 	}
@@ -46,25 +47,47 @@ public:
 		_Alloc((typename index_type::pointer)_Shape.data());
 	}
 
+	/**
+	 *\brief re-create multi_array object from its built-in index type.
+	 */
 	MATRICE_HOST_INL _Myt& resize(const index_type& _Shape) {
 		_Alloc(_Shape);
 		return (*this);
 	}
+	/**
+	 *\brief re-create multi_array object from std::array<>.
+	 */
 	MATRICE_HOST_INL _Myt& resize(const std::array<size_t, dimension>& _Shape) {
 		_Alloc((typename index_type::pointer)_Shape.data());
 		return (*this);
 	}
+
+	/**
+	 *\brief get the number of elements.
+	 */
 	MATRICE_HOST_INL size_t size() const noexcept {
 		return _Data.size();
 	}
+
+	/**
+	 *\brief get data pointer.
+	 */
 	MATRICE_HOST_INL pointer data() noexcept { return _Data.data(); }
 	MATRICE_HOST_INL const pointer data() const noexcept { return _Data.data(); }
+
+	/**
+	 *\brief random accessor with element index.
+	 */
 	MATRICE_HOST_INL reference operator()(const index_type& _idx) {
 		return _Data.data()[_Index(_idx)];
 	}
 	MATRICE_HOST_INL const reference operator()(const index_type& _idx) const {
 		return _Data.data()[_Index(_idx)];
 	}
+
+	/**
+	 *\brief linear random accessor.
+	 */
 	MATRICE_HOST_INL reference operator[](size_t _idx) noexcept {
 		return _Data.data()[_idx];
 	}
@@ -72,29 +95,35 @@ public:
 		return _Data.data()[_idx];
 	}
 
-	MATRICE_HOST_INL typename _Mybuf::iterator begin() noexcept {
+	/**
+	 *\brief element-wise style iterator begin.
+	 */
+	MATRICE_HOST_INL typename _Mybuf_t::iterator begin() noexcept {
 		return _Data.begin(); 
 	}
-	MATRICE_HOST_INL typename _Mybuf::const_iterator begin() const noexcept {
+	MATRICE_HOST_INL typename _Mybuf_t::const_iterator begin() const noexcept {
 		return _Data.begin(); 
 	}
-	MATRICE_HOST_INL typename _Mybuf::iterator end() noexcept {
+	/**
+	 *\brief element-wise style iterator end.
+	 */
+	MATRICE_HOST_INL typename _Mybuf_t::iterator end() noexcept {
 		return _Data.end(); 
 	}
-	MATRICE_HOST_INL typename _Mybuf::const_iterator end() const noexcept { 
+	MATRICE_HOST_INL typename _Mybuf_t::const_iterator end() const noexcept {
 		return _Data.end(); 
 	}
 
 	/**
 	 *\brief retrieve data buffer by lvalue reference.
 	 */
-	MATRICE_HOST_INL _Mybuf& all() & {
+	MATRICE_HOST_INL _Mybuf_t& all()& noexcept {
 		return _Data; 
 	}
 	/**
 	 *\brief retrieve data buffer by rvalue reference.
 	 */
-	MATRICE_HOST_INL _Mybuf all() && { 
+	MATRICE_HOST_INL _Mybuf_t all()&& noexcept {
 		return move(_Data); 
 	}
 
@@ -110,13 +139,13 @@ private:
 		else
 			_Data.create(_Size, zero<value_type>);
 	}
-	MATRICE_HOST_INL size_t _Index(const index_type& _index)const noexcept {
+	MATRICE_HOST_INL size_t _Index(const index_type& _index) const noexcept {
 		return ((_Stride*_index).sum());
 	}
 
 private:
 	array_n<int, dimension> _Stride;
-	_Mybuf _Data;
+	_Mybuf_t _Data;
 };
 
 DGE_MATRICE_END
