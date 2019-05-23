0d9c8f0e359187e6ab49b7e8caf90cef0496e0b0
Zhilong Su <su-zl@seu.edu.cn>
2019-05-23 22:33:54 +0800

    Add constexpr support.

--- Matrice/include/Matrice/private/_matrix_shape.hpp
+++ Matrice/include/Matrice/private/_matrix_shape.hpp
@@ -47,14 +47,14 @@ public:
 	using hist_shape = tuple<value_type,view_shape>;
 	using full_shape = tuple<value_type,hist_shape>;
 
-	MATRICE_GLOBAL_INL basic_shape(shape_t<value_type>&& _Shape) noexcept
+	MATRICE_GLOBAL_INL constexpr basic_shape(shape_t<value_type>&& _Shape) noexcept
 		: _Data{ 1,{1,_Shape} } {}
-	MATRICE_GLOBAL_INL basic_shape(shape3_t<value_type>&& _Shape) noexcept
+	MATRICE_GLOBAL_INL constexpr basic_shape(shape3_t<value_type>&& _Shape) noexcept
 		: _Data{ 1,{std::get<0>(_Shape), {std::get<1>(_Shape),std::get<2>(_Shape)}} } {}
-	MATRICE_GLOBAL_INL basic_shape(shape4_t<value_type>&& _Shape) noexcept
+	MATRICE_GLOBAL_INL constexpr basic_shape(shape4_t<value_type>&& _Shape) noexcept
 		: _Data{ std::get<0>(_Shape), {std::get<1>(_Shape), {std::get<2>(_Shape),std::get<3>(_Shape)}} } {}
 	template<typename _Jty>
-	MATRICE_GLOBAL_INL basic_shape(std::initializer_list<_Jty> _Shape) noexcept {
+	MATRICE_GLOBAL_INL constexpr basic_shape(const initlist<_Jty> _Shape) noexcept {
 		if (_Shape.size() == 2) {
 			_Data = { 1,{1,{(value_type)*_Shape.begin(), (value_type)*(_Shape.begin() + 1)}} };
 		}
@@ -65,18 +65,18 @@ public:
 			_Data = { (value_type)*_Shape.begin(),{(value_type)*(_Shape.begin() + 1),{(value_type)*(_Shape.begin() + 2), (value_type)*(_Shape.begin() + 3)}} };
 		}
 	}
-	MATRICE_GLOBAL_INL basic_shape(const _Myt& _Other) noexcept
+	MATRICE_GLOBAL_INL constexpr basic_shape(const _Myt& _Other) noexcept
 		: _Data(_Other._Data) {}
-	MATRICE_GLOBAL_INL basic_shape(_Myt&& _Other) noexcept
-		: _Data(std::move(_Other._Data)) {}
+	MATRICE_GLOBAL_INL constexpr basic_shape(_Myt&& _Other) noexcept
+		: _Data(move(_Other._Data)) {}
 
-	MATRICE_GLOBAL_INL auto& operator= (const _Myt& _Oth) {
+	MATRICE_GLOBAL_INL constexpr decltype(auto) operator= (const _Myt& _Oth) noexcept {
 		_Data = (_Oth._Data); return (*this);
 	}
-	MATRICE_GLOBAL_INL auto& operator= (_Myt&& _Oth) {
-		_Data = std::move(_Oth._Data); return (*this);
+	MATRICE_GLOBAL_INL constexpr decltype(auto) operator= (_Myt&& _Oth) noexcept {
+		_Data = move(_Oth._Data); return (*this);
 	}
-	MATRICE_GLOBAL_INL auto& operator= (const std::initializer_list<size_t> _Shape) {
+	MATRICE_GLOBAL_INL constexpr decltype(auto) operator= (const initlist<size_t> _Shape) noexcept {
 		if (_Shape.size() == 2) {
 			_Data = { 1,{1,{(value_type)*_Shape.begin(), (value_type)*(_Shape.begin() + 1)}} };
 		}
@@ -91,43 +91,43 @@ public:
 	/**
 	 *\brief Get unrolled shape data
 	 */
-	MATRICE_GLOBAL_INL auto operator()() const {
+	MATRICE_GLOBAL_INL constexpr decltype(auto) operator()() const noexcept {
 		return std::make_tuple(get(0), get(1), get(2), get(3));
 	}
 	/**
 	 *\brief Get full rows and cols to a shape_t
 	 */
-	MATRICE_GLOBAL_INL operator shape_t<size_t>() const {
+	MATRICE_GLOBAL_INL constexpr operator shape_t<size_t>() const noexcept {
 		return shape_t<size_t>(rows(), cols());
 	}
 	/**
 	 *\brief Comparation operators
 	 */
-	MATRICE_GLOBAL_INL friend bool operator== (const _Myt& _lhs, const _Myt& _rhs) {
+	MATRICE_GLOBAL_INL friend bool operator==(const _Myt& _lhs, const _Myt& _rhs) {
 		return (_lhs._Data == _rhs._Data);
 	}
 	template<typename _Rhs>
-	MATRICE_GLOBAL_INL friend bool operator== (const _Myt& _lhs, const _Rhs& _rhs) {
+	MATRICE_GLOBAL_INL friend bool operator==(const _Myt& _lhs, const _Rhs& _rhs) {
 		return (_lhs._Data == _Myt(_rhs)._Data);
 	}
-	MATRICE_GLOBAL_INL friend bool operator!= (const _Myt& _lhs, const _Myt& _rhs) {
+	MATRICE_GLOBAL_INL friend bool operator!=(const _Myt& _lhs, const _Myt& _rhs) {
 		return (_lhs._Data != _rhs._Data);
 	}
 	template<typename _Rhs>
-	MATRICE_GLOBAL_INL friend bool operator!= (const _Myt& _lhs, const _Rhs& _rhs) {
+	MATRICE_GLOBAL_INL friend bool operator!=(const _Myt& _lhs, const _Rhs& _rhs) {
 		return (_lhs._Data != _Myt(_rhs)._Data);
 	}
 	/**
 	 *\brief Get dim value at _Dim
 	 */
-	MATRICE_GLOBAL_INL constexpr auto& get(uint8_t _Dim) {
+	MATRICE_GLOBAL_INL constexpr auto& get(uint8_t _Dim) noexcept {
 		if (_Dim == 0) return std::get<0>(_Data);
 		if (_Dim == 1) return std::get<0>(std::get<1>(_Data));
 		if (_Dim == 2) return std::get<0>(std::get<1>(std::get<1>(_Data)));
 		if (_Dim == 3) return std::get<1>(std::get<1>(std::get<1>(_Data)));
 		DGELOM_CHECK(_Dim<3, "_Dim over range of _Data.");
 	}
-	MATRICE_GLOBAL_INL constexpr auto& get(uint8_t _Dim) const {
+	MATRICE_GLOBAL_INL constexpr const auto& get(uint8_t _Dim) const noexcept {
 		if (_Dim == 0) return std::get<0>(_Data);
 		if (_Dim == 1) return std::get<0>(std::get<1>(_Data));
 		if (_Dim == 2) return std::get<0>(std::get<1>(std::get<1>(_Data)));
@@ -137,39 +137,39 @@ public:
 	/**
 	 *\brief Get full rows
 	 */
-	MATRICE_GLOBAL_INL constexpr auto rows() const {
+	MATRICE_GLOBAL_INL constexpr auto (rows)() const noexcept {
 		return get(0) * get(2);
 	}
 	/**
 	 *\brief Get full cols
 	 */
-	MATRICE_GLOBAL_INL constexpr auto cols() const {
+	MATRICE_GLOBAL_INL constexpr auto (cols)() const noexcept {
 		return get(1) * get(3);
 	}
 	/**
 	 *\brief return C*H*W
 	 */
-	MATRICE_GLOBAL_INL constexpr auto chw() const {
+	MATRICE_GLOBAL_INL constexpr auto (chw)() const noexcept {
 		return (get(1)*hw());
 	}
 	/**
 	 *\brief return H*W
 	 */
-	MATRICE_GLOBAL_INL constexpr auto hw() const {
+	MATRICE_GLOBAL_INL constexpr auto (hw)() const noexcept {
 		return (get(2)*get(3));
 	}
 	/**
 	 *\brief Parse the index for each dimension from a linear index
 	 *\param [_Idx] input linear index
 	 */
-	MATRICE_GLOBAL_INL constexpr auto parse(size_t _Idx) const {
+	MATRICE_GLOBAL_INL constexpr auto parse(size_t _Idx) const noexcept {
 		auto n = _Idx / chw();  _Idx -= n * chw();
 		auto c = _Idx / hw();   _Idx -= c * hw();
 		auto h = _Idx / get(3); _Idx -= h * get(3);
 		return shape4_t<>(n, c, h, _Idx);
 	}
 
-	friend MATRICE_GLOBAL_INL _Myt _Union(const _Myt& _1, const _Myt& _2) {
+	friend MATRICE_GLOBAL_INL _Myt _Union(const _Myt& _1, const _Myt& _2) noexcept {
 		return {max(_1.get(0), _2.get(0)), max(_1.get(1), _2.get(1)),
 		max(_1.get(2), _2.get(2)), max(_1.get(3), _2.get(3)) };
 	}
