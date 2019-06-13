c83cab3e64b8f5cc52dead5676adebdb2d722da4
Zhilong Su <su-zl@seu.edu.cn>
2019-06-13 16:07:18 +0800

    Update _primitive_funcs.hpp

--- Matrice/include/Matrice/private/math/_primitive_funcs.hpp
+++ Matrice/include/Matrice/private/math/_primitive_funcs.hpp
@@ -25,37 +25,94 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 DGE_MATRICE_BEGIN
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
-MATRICE_GLOBAL_FINL constexpr _Ret add(const T1& a, const T2& b) { return a + b; }
+MATRICE_GLOBAL_FINL constexpr _Ret add(const T1& a, const T2& b) { 
+	return a + b; 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
-MATRICE_GLOBAL_FINL constexpr _Ret sub(const T1& a, const T2& b) { return a - b; }
+MATRICE_GLOBAL_FINL constexpr _Ret sub(const T1& a, const T2& b) { 
+	return a - b; 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
-MATRICE_GLOBAL_FINL constexpr _Ret mul(const T1& a, const T2& b) { return a * b; }
+MATRICE_GLOBAL_FINL constexpr _Ret mul(const T1& a, const T2& b) { 
+	return a * b; 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
-MATRICE_GLOBAL_FINL constexpr _Ret div(const T1& a, const T2& b) { return a / b; }
+MATRICE_GLOBAL_FINL constexpr _Ret div(const T1& a, const T2& b) { 
+	return a / b; 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>> 
-MATRICE_GLOBAL_FINL constexpr _Ret max(const T1& a, const T2& b) { return a < b ? b : a; }
+MATRICE_GLOBAL_FINL constexpr _Ret max(const T1& a, const T2& b) { 
+	return a < b ? b : a; 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
-MATRICE_GLOBAL_FINL constexpr _Ret min(const T1& a, const T2& b) { return a < b ? a : b; }
+MATRICE_GLOBAL_FINL constexpr _Ret min(const T1& a, const T2& b) { 
+	return a < b ? a : b; 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>> 
-MATRICE_GLOBAL_FINL constexpr _Ret sqrt(const T& x) { return std::sqrt(x); }
+MATRICE_GLOBAL_FINL constexpr _Ret sqrt(const T& x) { 
+	return std::sqrt(x); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>> 
-MATRICE_HOST_FINL constexpr _Ret abs(const T& x) { return std::abs(x); }
+MATRICE_HOST_FINL constexpr _Ret abs(const T& x) { 
+	return std::abs(x); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>>
-MATRICE_HOST_FINL constexpr _Ret exp(const T& x) { return std::exp(x); }
+MATRICE_HOST_FINL constexpr _Ret exp(const T& x) { 
+	return std::exp(x); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>>
-MATRICE_HOST_FINL constexpr _Ret log(const T& x) { return std::log(x); }
+MATRICE_HOST_FINL constexpr _Ret log(const T& x) { 
+	return std::log(x); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>>
-MATRICE_HOST_FINL constexpr _Ret log2(const T& x) { return std::log2(x); }
+MATRICE_HOST_FINL constexpr _Ret log2(const T& x) { 
+	return std::log2(x); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>>
-MATRICE_HOST_FINL constexpr _Ret log10(const T& x) { return std::log10(x); }
+MATRICE_HOST_FINL constexpr _Ret log10(const T& x) { 
+	return std::log10(x); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>>
-MATRICE_HOST_FINL constexpr _Ret floor(const T& x) { return static_cast<_Ret>(std::floor(x)); }
+MATRICE_HOST_FINL constexpr _Ret floor(const T& x) { 
+	return static_cast<_Ret>(std::floor(x)); 
+}
 template<typename T, typename _Ret = T, typename = enable_if_t<is_arithmetic_v<T>>> 
-MATRICE_HOST_FINL constexpr _Ret ceil(const T& x) { return static_cast<_Ret>(std::ceil(x)); }
+MATRICE_HOST_FINL constexpr _Ret ceil(const T& x) { 
+	return static_cast<_Ret>(std::ceil(x)); 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
-MATRICE_GLOBAL_FINL constexpr _Ret pow(const T1& x, const T2& y) { return std::pow(x,y); }
+MATRICE_GLOBAL_FINL constexpr _Ret pow(const T1& x, const T2& y) { 
+	return std::pow(x,y); 
+}
 template<typename T1, typename T2, typename _Ret = common_type_t<T1, T2>, typename = enable_if_t<is_arithmetic_v<_Ret>>>
 MATRICE_GLOBAL_FINL constexpr _Ret safe_div(const T1& a, const T2& b) { 
 	return b == T2(0) ? _Ret(0) : div(a, b);
 }
+
+///<brief> trigonometric functions </brief>
+template<typename T, enable_if_t<is_arithmetic_v<T>> = 0>
+MATRICE_GLOBAL_FINL constexpr T sin(const T& x) noexcept {
+	return std::sin(x);
+}
+template<typename T, enable_if_t<is_arithmetic_v<T>> = 0>
+MATRICE_GLOBAL_FINL constexpr T cos(const T& x) noexcept {
+	return std::cos(x);
+}
+template<typename T, enable_if_t<is_arithmetic_v<T>> = 0>
+MATRICE_GLOBAL_FINL constexpr T tan(const T& x) noexcept {
+	return std::tan(x);
+}
+template<typename T, enable_if_t<is_arithmetic_v<T>> = 0>
+MATRICE_GLOBAL_FINL constexpr T arcsin(const T& x) noexcept {
+	return std::asin(x);
+}
+template<typename T, enable_if_t<is_arithmetic_v<T>> = 0>
+MATRICE_GLOBAL_FINL constexpr T arccos(const T& x) noexcept {
+	return std::acos(x);
+}
+template<typename T, enable_if_t<is_arithmetic_v<T>> = 0>
+MATRICE_GLOBAL_FINL constexpr T arctan(const T& x) noexcept {
+	return std::atan(x);
+}
+
 DGE_MATRICE_END
