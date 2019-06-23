4d1bcccb7c9899464a616cbdf8fdf51803ad3404
Zhilong Su <su-zl@seu.edu.cn>
2019-06-23 17:48:45 +0800

    Add aligned projection class.

--- Matrice/include/Matrice/algs/stereovision/_projection.hpp
+++ Matrice/include/Matrice/algs/stereovision/_projection.hpp
@@ -18,6 +18,9 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #include "../../../core"
 DGE_MATRICE_BEGIN
+struct cs_alignment_tag { 
+	struct left {}; struct middle {}; struct right{};
+};
 _DETAIL_BEGIN
 template<typename _Ty> class _Projection_base {
 	using _Myt = _Projection_base<_Ty>;
@@ -26,13 +29,18 @@ public:
 	using value_type = _Ty;
 	using point_type = Vec3_<value_type>;
 	using matrix_type = Matrix_<value_type, dim, dim>;
-
+	template<typename _An> struct depth_paramterized_projection {
+		point_type point;
+		MATRICE_HOST_INL point_type grad() noexcept {
+			return point_type{};
+		}
+	};
 	/**
 	 *\brief initialize the rotation and translation
 	 *\param [_Ext] external geometry parameters: rx, ry, rz, tx, ty, tz
 	 */
-	_Projection_base(const array_n<value_type, dim<<1>& _Ext)
-	 :_MyT(_Ext(3), _Ext(4), _Ext(5)) {
+	_Projection_base(const array_n<value_type, dim<<1>& _Ext) noexcept
+		:_MyT(_Ext(3), _Ext(4), _Ext(5)) {
 		point_type r{ _Ext(0), _Ext(1), _Ext(2) };
 		const auto theta = sqrt(r.dot(r));
 		const auto sval = sin(theta);
@@ -40,9 +48,15 @@ public:
 
 		_MyR.identity(); cval = 1 - cval; r = r / theta;
 
-		_MyR(0) += cval * r[0] * r[0], _MyR(1) += cval * r[0] * r[1], _MyR(2) += cval * r[0] * r[2];
-		_MyR(3) += cval * r[0] * r[1], _MyR(4) += cval * r[1] * r[1], _MyR(5) += cval * r[1] * r[2];
-		_MyR(6) += cval * r[0] * r[2], _MyR(7) += cval * r[1] * r[2], _MyR(8) += cval * r[2] * r[2];
+		_MyR(0) += cval * r[0] * r[0];
+		_MyR(1) += cval * r[0] * r[1];
+		_MyR(2) += cval * r[0] * r[2];
+		_MyR(3) += cval * r[1] * r[0];
+		_MyR(4) += cval * r[1] * r[1];
+		_MyR(5) += cval * r[1] * r[2];
+		_MyR(6) += cval * r[2] * r[0];
+		_MyR(7) += cval * r[2] * r[1];
+		_MyR(8) += cval * r[2] * r[2];
 
 		_MyR(1) -= sval * r[2], _MyR(2) += sval * r[1];
 		_MyR(3) += sval * r[2], _MyR(5) -= sval * r[0];
@@ -50,10 +64,10 @@ public:
 	}
 
 	/**
-	 *\brief transform X with [x, y, z]^T = RX + T
+	 *\brief rotation X with [x, y, z]^T = RX
 	 *\param [_X] input 3d point
 	 */
-	MATRICE_HOST_INL point_type transform(const point_type& _X)noexcept{
+	MATRICE_HOST_INL point_type rotation(const point_type& _X)noexcept{
 		return (_MyR.mul(_X) + _MyT);
 	}
 	/**
@@ -61,13 +75,50 @@ public:
 	 *\param [_X] input 3d point
 	 */
 	MATRICE_HOST_INL point_type forward(const point_type& _X)noexcept{
-		point_type p = this->transform(_X);
+		point_type p = this->rotation(_X) + _MyT;
 		return (p.normalize(p.z));
 	}
 
 protected:
 	matrix_type _MyR;
 	point_type _MyT;
+};
+
+template<typename _Ty, typename _An> class _Aligned_projection {
+	static_assert("");
+};
+
+template<typename _Ty>
+class _Aligned_projection<_Ty, cs_alignment_tag::left>
+	:public _Projection_base<_Ty> {
+	using _Myt = _Aligned_projection;
+	using _Mybase = _Projection_base<_Ty>;
+public:
+	using _Mybase::_Projection_base;
+	using typename _Mybase::point_type;
+	using typename _Mybase::value_type;
+
+	/**
+	 *\brief compute the gradient of projected point w.r.t. depth
+	 *\param [pd] the first two elements are coordinates of a normalized, distortion-rectified image point, the last one is depth. 
+	 */
+	MATRICE_HOST_INL point_type backward(const point_type& pd) noexcept {
+		const auto d = pd.z; //depth
+		const auto X = _Mybase::rotation({ pd.x, pd.y, 1 });
+		const auto s = 1 / sqr(X.z*d + _MyT.z);
+		const auto gx = (X.x*_MyT.z - X.z*_MyT.x)*s;
+		const auto gy = (X.y*_MyT.z - X.z*_MyT.y)*s;
+		return point_type{ gx, gy, 0 };
+	}
+	MATRICE_HOST_INL point_type backward(value_type x, value_type y, value_type depth) noexcept {
+		const auto X = _Mybase::rotation({ x, y, 1 });
+		const auto s = 1 / sqr(X.z*depth + _MyT.z);
+		const auto gx = (X.x*_MyT.z - X.z*_MyT.x)*s;
+		const auto gy = (X.y*_MyT.z - X.z*_MyT.y)*s;
+		return point_type{ gx, gy, 0 };
+	}
+private:
+	using _Mybase::_MyT;
 };
 _DETAIL_END
 
