9c627143540a6fa83bbbf4a12e3c8dd77ad07f49
Zhilong Su <su-zl@seu.edu.cn>
2019-06-25 13:49:22 +0800

    Update tensor.h

--- Matrice/include/Matrice/core/tensor.h
+++ Matrice/include/Matrice/core/tensor.h
@@ -1,15 +1,32 @@
+/**************************************************************************
+This file is part of Matrice, an effcient and elegant C++ library.
+Copyright(C) 2018-2019, Zhilong(Dgelom) Su, all rights reserved.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
 #pragma once
 
-#include "../private/tensor/_tensor.h"
 #include "../private/tensor/_tensor.hpp"
+//#include "../private/tensor/_tensor.hpp"
 
 DGE_MATRICE_BEGIN
 template<typename _Ty>
 using tensor = detail::_Tensor<_Ty>;
 
-template<typename _Ty, size_t... _Shape>
-using tensor_ = detail::_Tensor_impl<_Ty, _Shape...>;
+//template<typename _Ty, size_t... _Shape>
+//using tensor_ = detail::_Tensor_impl<_Ty, _Shape...>;
 
-template<typename _Ty, int... _Shape>
-using multi_matrix = detail::_Multi_matrix<_Ty, _Shape...>;
+//template<typename _Ty, int... _Shape>
+//using multi_matrix = detail::_Multi_matrix<_Ty, _Shape...>;
 DGE_MATRICE_END
