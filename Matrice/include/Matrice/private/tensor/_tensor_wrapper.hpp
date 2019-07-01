38bc14945990ae8afa92a53c8068d9fc7b69e98b
Zhilong Su <su-zl@seu.edu.cn>
2019-07-01 14:28:17 +0800

    Create _tensor_wrapper.hpp

---
+++ Matrice/include/Matrice/private/tensor/_tensor_wrapper.hpp
@@ -0,0 +1,40 @@
+/*********************************************************************
+This file is part of Matrice, an effcient and elegant C++ library.
+Copyright(C) 2018-2019, Zhilong(Dgelom) Su, all rights reserved.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
+#pragma once
+#include "_tensor.hpp"
+
+DGE_MATRICE_BEGIN
+
+/* tensor instance */
+template<typename _Ty, typename dev_tag> 
+struct tensor_inst {};
+
+/* tensor descriptor */
+struct tensor_desc {
+	uint32_t depth;
+	uint32_t extent;
+	size_t height, width;
+};
+
+template<typename _Ty>
+class tensor_view {
+public:
+private:
+	tensor_desc m_desc;
+};
+DGE_MATRICE_END
