5c7d6680a138d628b672ebf3388e16eb25b8e8ac
Zhilong Su <su-zl@seu.edu.cn>
2019-07-04 17:26:29 +0800

    Update speckle_deformation_simulation_ex.hpp

--- Matrice/examples/speckle_deformation_simulation_ex.hpp
+++ Matrice/examples/speckle_deformation_simulation_ex.hpp
@@ -1,5 +1,6 @@
 
 #pragma once
+#include <optional>
 #include "../include/io"
 #include "../include/core"
 #include "../include/Matrice/algs/interpolation.h"
@@ -19,22 +20,43 @@ public:
 	}
 
 	template<typename _Fn>
-	void load_src(_Fn&& op) noexcept {
-		m_original = std::move(op);
+	inline void load_src(_Fn&& op) noexcept {
+		const auto src = op();
+		if constexpr (is_same_v<decltype(src), matrix_type>) {
+			m_original = move(src);
+		}
+		else {
+			m_original.create(src.shape());
+			for (const auto& idx : range(0, src.size()))
+				m_original(idx) = src(idx);
+		}
 		const auto x = (m_original.cols() - m_cols) / 2;
 		const auto y = (m_original.rows() - m_rows) / 2;
 		m_rect.set({ x, y }, m_cols, m_rows);
 	}
 
+	inline decltype(auto) original() noexcept {
+		return (m_original);
+	}
+
+	inline decltype(auto) get_block_range() const noexcept {
+		return std::make_tuple(m_rect.begin().x, m_rect.end().x, m_rect.begin().y, m_rect.end().y);
+	}
+
 	template<uint16_t _Order>
-	matrix_type random_warp() noexcept {
-		
+	inline std::optional<matrix_type> random_warp() noexcept {
+		mt19937 eng;
+		normal_distribution<value_type> ndist;
+		interpolation<value_type, tag::bicspl_tag> itp(m_original);
+
 		matrix_type dst(m_rows, m_cols);
 		if constexpr (_Order == static_cast<uint16_t>(0)) {
 			for (const auto& idx : range(0, dst.size())) {
-				const auto r = idx / m_cols;
-				const auto c = idx - r * m_cols;
-
+				auto [r, c] = unroll_linear_index<int>(idx, m_cols);
+				const auto u = ndist(eng), v = ndist(eng);
+				const auto x = c + m_rect.begin().x - u;
+				const auto y = r + m_rect.begin().y - v;
+				dst[r][c] = itp(x, y);
 			}
 		}
 		else if constexpr(_Order == static_cast<uint16_t>(1)) {
@@ -44,10 +66,10 @@ public:
 
 		}
 		else {
-			static_assert("Unsupported deformation order.")
+			static_assert("Unsupported deformation order.");
 		}
 
-		return dst;
+		return std::make_optional(dst);
 	}
 
 private:
