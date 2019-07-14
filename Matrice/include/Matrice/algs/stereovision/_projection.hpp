a5dc28eea8e0ba4cafe5a836f1a25c01c0dc3fcd
Zhilong Su <su-zl@seu.edu.cn>
2019-07-14 17:03:34 +0800

    Update _projection.hpp

--- Matrice/include/Matrice/algs/stereovision/_projection.hpp
+++ Matrice/include/Matrice/algs/stereovision/_projection.hpp
@@ -37,51 +37,51 @@ public:
 	};
 	/**
 	 *\brief initialize the rotation and translation
-	 *\param [_Ext] external geometry parameters: rx, ry, rz, tx, ty, tz
+	 *\param [ext] external geometry parameters: rx, ry, rz, tx, ty, tz
 	 */
-	_Projection_base(const array_n<value_type, dim<<1>& _Ext) noexcept
-		:_MyT(_Ext(3), _Ext(4), _Ext(5)) {
-		point_type r{ _Ext(0), _Ext(1), _Ext(2) };
+	_Projection_base(const array_n<value_type, dim<<1>& ext) noexcept
+		:m_tran(ext(3), ext(4), ext(5)) {
+		point_type r{ ext(0), ext(1), ext(2) };
 		const auto theta = sqrt(r.dot(r));
 		const auto sval = sin(theta);
 		auto cval = cos(theta);
 
-		_MyR.identity(); cval = 1 - cval; r = r / theta;
+		m_rotm.identity(); cval = 1 - cval; r = r / theta;
 
-		_MyR(0) += cval * r[0] * r[0];
-		_MyR(1) += cval * r[0] * r[1];
-		_MyR(2) += cval * r[0] * r[2];
-		_MyR(3) += cval * r[1] * r[0];
-		_MyR(4) += cval * r[1] * r[1];
-		_MyR(5) += cval * r[1] * r[2];
-		_MyR(6) += cval * r[2] * r[0];
-		_MyR(7) += cval * r[2] * r[1];
-		_MyR(8) += cval * r[2] * r[2];
+		m_rotm(0) += cval * r[0] * r[0];
+		m_rotm(1) += cval * r[0] * r[1];
+		m_rotm(2) += cval * r[0] * r[2];
+		m_rotm(3) += cval * r[1] * r[0];
+		m_rotm(4) += cval * r[1] * r[1];
+		m_rotm(5) += cval * r[1] * r[2];
+		m_rotm(6) += cval * r[2] * r[0];
+		m_rotm(7) += cval * r[2] * r[1];
+		m_rotm(8) += cval * r[2] * r[2];
 
-		_MyR(1) -= sval * r[2], _MyR(2) += sval * r[1];
-		_MyR(3) += sval * r[2], _MyR(5) -= sval * r[0];
-		_MyR(6) -= sval * r[1], _MyR(7) += sval * r[0];
+		m_rotm(1) -= sval * r[2], m_rotm(2) += sval * r[1];
+		m_rotm(3) += sval * r[2], m_rotm(5) -= sval * r[0];
+		m_rotm(6) -= sval * r[1], m_rotm(7) += sval * r[0];
 	}
 
 	/**
 	 *\brief rotation X with [x, y, z]^T = RX
 	 *\param [_X] input 3d point
 	 */
-	MATRICE_HOST_INL point_type rotation(const point_type& _X)noexcept{
-		return (_MyR.mul(_X) + _MyT);
+	MATRICE_HOST_INL point_type rotation(const point_type& _X) noexcept{
+		return (m_rotm.mul(_X) + m_tran);
 	}
 	/**
 	 *\brief transform and normalize with [x, y, 1]^T = <RX + T>
 	 *\param [_X] input 3d point
 	 */
-	MATRICE_HOST_INL point_type forward(const point_type& _X)noexcept{
-		point_type p = this->rotation(_X) + _MyT;
+	MATRICE_HOST_INL point_type forward(const point_type& _X) noexcept{
+		point_type p = this->rotation(_X) + m_tran;
 		return (p.normalize(p.z));
 	}
 
 protected:
-	matrix_type _MyR;
-	point_type _MyT;
+	matrix_type m_rotm;
+	point_type m_tran;
 };
 
 template<typename _Ty, typename _An> class _Aligned_projection {
@@ -90,7 +90,7 @@ template<typename _Ty, typename _An> class _Aligned_projection {
 
 template<typename _Ty>
 class _Aligned_projection<_Ty, cs_alignment_tag::left>
-	:public _Projection_base<_Ty> {
+	: public _Projection_base<_Ty> {
 	using _Myt = _Aligned_projection;
 	using _Mybase = _Projection_base<_Ty>;
 public:
@@ -99,26 +99,25 @@ public:
 	using typename _Mybase::value_type;
 
 	/**
-	 *\brief compute the gradient of projected point w.r.t. depth
-	 *\param [pd] the first two elements are coordinates of a normalized, distortion-rectified image point, the last one is depth. 
+	 *\brief compute the grad. of a re-projected point w.r.t. depth par.
+	 *\param [pd] re-projected point, where the first two elements are coordinates of a normalized, distortion-rectified image point, the last one is its depth value.
 	 */
-	MATRICE_HOST_INL point_type backward(const point_type& pd) noexcept {
-		const auto d = pd.z; //depth
+	MATRICE_HOST_INL point_type grad(const point_type& pd) noexcept {
 		const auto X = _Mybase::rotation({ pd.x, pd.y, 1 });
-		const auto s = 1 / sqr(X.z*d + _MyT.z);
-		const auto gx = (X.x*_MyT.z - X.z*_MyT.x)*s;
-		const auto gy = (X.y*_MyT.z - X.z*_MyT.y)*s;
+		const auto s = 1 / sqr(X.z*pd.z + m_tran.z);
+		const auto gx = (X.x*m_tran.z - X.z*m_tran.x)*s;
+		const auto gy = (X.y*m_tran.z - X.z*m_tran.y)*s;
 		return point_type{ gx, gy, 0 };
 	}
-	MATRICE_HOST_INL point_type backward(value_type x, value_type y, value_type depth) noexcept {
+	MATRICE_HOST_INL point_type grad(value_type x, value_type y, value_type depth) noexcept {
 		const auto X = _Mybase::rotation({ x, y, 1 });
-		const auto s = 1 / sqr(X.z*depth + _MyT.z);
-		const auto gx = (X.x*_MyT.z - X.z*_MyT.x)*s;
-		const auto gy = (X.y*_MyT.z - X.z*_MyT.y)*s;
+		const auto s = 1 / sqr(X.z*depth + m_tran.z);
+		const auto gx = (X.x*m_tran.z - X.z*m_tran.x)*s;
+		const auto gy = (X.y*m_tran.z - X.z*m_tran.y)*s;
 		return point_type{ gx, gy, 0 };
 	}
 private:
-	using _Mybase::_MyT;
+	using _Mybase::m_tran;
 };
 _DETAIL_END
 
