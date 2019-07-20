ed460fbc2168e37f813761191ace3e350045c45b
Zhilong Su <su-zl@seu.edu.cn>
2019-07-20 21:12:11 +0800

    Update type aliases.

--- Matrice/include/Matrice/private/_storage.hpp
+++ Matrice/include/Matrice/private/_storage.hpp
@@ -52,11 +52,11 @@ template<typename _Ty> class Storage_
 {
 public:
 #ifdef __CXX11__
-	using value_t   =             _Ty;
-	using pointer   =            _Ty*;
-	using reference =            _Ty&;
-	using int_t     =  std::ptrdiff_t;
-	using idx_t     =          size_t;
+	using value_t = _Ty;
+	using pointer = std::add_pointer_t<value_t>;
+	using reference = std::add_lvalue_reference_t<value_t>;
+	using int_t = std::ptrdiff_t;
+	using idx_t = size_t;
 #elif
 	typedef _Ty               value_t;
 	typedef _Ty*              pointer;
