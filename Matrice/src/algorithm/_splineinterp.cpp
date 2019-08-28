bbd7b3ed3afb68e66abadaab07d7ca1f22559f4c
Zhilong Su <su-zl@seu.edu.cn>
2019-08-28 13:39:15 +0800

    Add license.

--- Matrice/src/algorithm/_splineinterp.cpp
+++ Matrice/src/algorithm/_splineinterp.cpp
@@ -1,4 +1,21 @@
-#include "../../include/Matrice/algs/interpolation/_splineinterp.h"
+/**************************************************************************
+This file is part of Matrice, an effcient and elegant C++ library.
+Copyright(C) 2018-2019, Zhilong(Dgelom) Su, all rights reserved.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+#include "algs/interpolation/_splineinterp.h"
 
 MATRICE_ALGS_BEGIN
 
