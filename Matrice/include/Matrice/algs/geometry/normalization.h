f93ff1d3fa1c087deb96ba31a8106ff0c71e8b11
Zhilong Su <su-zl@seu.edu.cn>
2019-12-15 15:33:35 +0800

    Clead code.

--- Matrice/include/Matrice/algs/geometry/normalization.h
+++ Matrice/include/Matrice/algs/geometry/normalization.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "../../core/matrix.h"
-#include "../../arch/ixpacket.h"
+#include "core/matrix.h"
+#include "arch/ixpacket.h"
 
 MATRICE_NAMESPACE_BEGIN_
 
-template<typename _Data_type, typename value_t = typename _Data_type::value_t> 
+template<typename _Data_type, typename value_t = typename _Data_type::value_t>
 class normalization MATRICE_NONHERITABLE
 {
 	using data_t = _Data_type;
@@ -18,15 +18,15 @@ public:
 	MATRICE_HOST_INL auto operator() () const {
 		if (_My_future.valid()) _My_future.get();
 
-		auto _Sx = std::sqrt(2/m_params(1)), _Sy = std::sqrt(2/m_params(3));
-		return types::Matrix_<value_t, 3, 3>{_Sx, 0, -_Sx * m_params(0),
+		auto _Sx = sqrt(2/m_params(1)), _Sy = sqrt(2/m_params(3));
+		return detail::Matrix_<value_t, 3, 3>{_Sx, 0, -_Sx * m_params(0),
 			0., _Sy, -_Sy * m_params(2), 0., 0., 1.};
 	}
 	MATRICE_HOST_INL auto operator() (data_t& _Data) const {
 		if (_My_future.valid()) _My_future.get();
 
 		auto _Mx = m_params(0), _My = m_params(2);
-		auto _Sx = std::sqrt(2/m_params(1)), _Sy = std::sqrt(2/m_params(3));
+		auto _Sx = sqrt(2/m_params(1)), _Sy = sqrt(2/m_params(3));
 
 		const auto _Size = m_data.cols();
 		std::transform(m_data[0], m_data[0] + _Size, _Data[0], [&](auto _X)->value_t { return (_Sx*(_X - _Mx)); });
@@ -34,7 +34,7 @@ public:
 	}
 private:
 	const data_t& m_data;
-	mutable types::Matrix_<value_t, 2, 2> m_params;
+	mutable detail::Matrix_<value_t, 2, 2> m_params;
 	mutable std::future<void> _My_future = std::async(std::launch::async, [&] {
 		const auto _N = m_data.cols();
 		m_params[0][0] = reduce<value_t>(m_data[0], m_data[0] + _N) / _N;
