5d40dfa2534d7b7e08e0ae8d765a2eb77afa0497
Zhilong Su <su-zl@seu.edu.cn>
2020-02-05 12:32:41 +0800

    Update geo_fwd.hpp

--- Matrice/include/Matrice/algs/geometry/geo_fwd.hpp
+++ Matrice/include/Matrice/algs/geometry/geo_fwd.hpp
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.If not, see <http://www.gnu.org/licenses/>.
 ***********************************************************************/
 #pragma once
+
+#include "Lie/_Lie_fwd.hpp"
+
 namespace dgelom {
 namespace detail {
 	template<typename _Ty> class _Quaternion;
