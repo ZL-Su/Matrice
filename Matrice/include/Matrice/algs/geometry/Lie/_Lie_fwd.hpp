821bc80f3c205c414544745800b25e8af063bfe6
Zhilong Su <su-zl@seu.edu.cn>
2020-02-05 16:10:17 +0800

    Update Lie type forwards.

--- Matrice/include/Matrice/algs/geometry/Lie/_Lie_fwd.hpp
+++ Matrice/include/Matrice/algs/geometry/Lie/_Lie_fwd.hpp
@@ -18,35 +18,44 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 namespace dgelom {
 namespace detail {
-	template<size_t _Dim, typename _Ty> class _SO;
-	template<size_t _Dim, typename _Ty> class _SE;
+	template<typename _Ty> class _SO2;
+	template<typename _Ty> class _SO3;
+	template<typename _Ty> class _SE2;
+	template<typename _Ty> class _SE3;
+	template<typename _Ty> class _se2;
+	template<typename _Ty> class _se3;
 }
 
 namespace internal {
 template<class _Group> struct _Lie_group_prop {};
+template<class _Algeb> struct _Lie_algebra_prop {};
 
 template<typename _Ty>
-struct _Lie_group_prop<detail::_SO<2, _Ty>> {
+struct _Lie_group_prop<detail::_SO2<_Ty>> {
 	static constexpr auto dim = 2;
 	static constexpr auto dof = 2;
+	using type = detail::_SO2<_Ty>;
 };
 
 template<typename _Ty>
-struct _Lie_group_prop<detail::_SO<3, _Ty>>{
+struct _Lie_group_prop<detail::_SO3<_Ty>>{
 	static constexpr auto dim = 3;
 	static constexpr auto dof = 3;
+	using type = detail::_SO3<_Ty>;
 };
 
 template<typename _Ty>
-struct _Lie_group_prop<detail::_SE<2, _Ty>> {
+struct _Lie_group_prop<detail::_SE2<_Ty>> {
 	static constexpr auto dim = 2;
 	static constexpr auto dof = 2;
+	using type = detail::_SE2<_Ty>;
 };
 
 template<typename _Ty>
-struct _Lie_group_prop<detail::_SE<3, _Ty>> {
+struct _Lie_group_prop<detail::_SE3<_Ty>> {
 	static constexpr auto dim = 3;
 	static constexpr auto dof = 3;
+	using type = detail::_SE2<_Ty>;
 };
 
 }
