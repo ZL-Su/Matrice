9158b9bf8763bd98cdab1736819b9661a2415aa9
Zhilong Su <su-zl@seu.edu.cn>
2020-04-19 18:29:33 +0800

    Update MKL supported blas and lapack kernels.

--- Matrice/src/private/nonfree/_blas_lapack_kernels.cpp
+++ Matrice/src/private/nonfree/_blas_lapack_kernels.cpp
@@ -20,63 +20,67 @@ decltype(auto) _blas_asum(const dptr x, size_t n, int inc) {
 
 template<>
 decltype(auto) _blas_axpy(const f32_t a, const fptr x, fptr y, size_t n, int incx, int incy) {
-	return cblas_saxpy(n, a, x, incx, y, incy);
+	return cblas_saxpy(MKL_INT(n), a, x, MKL_INT(incx), y, MKL_INT(incy));
 }
 template<>
 decltype(auto) _blas_axpy(const f64_t a, const dptr x, dptr y, size_t n, int incx, int incy) {
-	return cblas_daxpy(n, a, x, incx, y, incy);
+	return cblas_daxpy(MKL_INT(n), a, x, MKL_INT(incx), y, MKL_INT(incy));
 }
 
 template<>
 decltype(auto) _blas_dot(size_t n, const fptr x, int incx, const fptr y, int incy) {
-	return cblas_sdot(n, x, incx, y, incy);
+	return cblas_sdot(MKL_INT(n), x, MKL_INT(incx), y, MKL_INT(incy));
 }
 template<>
 decltype(auto) _blas_dot(size_t n, const dptr x, int incx, const dptr y, int incy) {
-	return cblas_ddot(n, x, incx, y, incy);
+	return cblas_ddot(MKL_INT(n), x, MKL_INT(incx), y, MKL_INT(incy));
 }
 
 template<>
 decltype(auto) _blas_nrm2(size_t n, const fptr x, int inc) {
-	return cblas_snrm2(n, x, inc);
+	return cblas_snrm2(MKL_INT(n), x, MKL_INT(inc));
 }
 template<>
 decltype(auto) _blas_nrm2(size_t n, const dptr x, int inc) {
-	return cblas_dnrm2(n, x, inc);
+	return cblas_dnrm2(MKL_INT(n), x, MKL_INT(inc));
 }
 
 template<>
 decltype(auto) _blas_scal(size_t n, const f32_t a, fptr x, int inc) {
-	return cblas_sscal(n, a, x, inc);
+	return cblas_sscal(MKL_INT(n), a, x, MKL_INT(inc));
 }
 template<>
 decltype(auto) _blas_scal(size_t n, const f64_t a, dptr x, int inc) {
-	return cblas_dscal(n, a, x, inc);
+	return cblas_dscal(MKL_INT(n), a, x, MKL_INT(inc));
 }
 
 template<>
 void _blas_gemv(size_t m, size_t n, f32_t a, const fptr A, const fptr x, int incx, f32_t b, fptr y, size_t incy) {
-	return cblas_sgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasNoTrans, m, n, a, A, (n > 1 ? n : 1), x, incx, b, y, incy);
+	return cblas_sgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasNoTrans, 
+		MKL_INT(m), MKL_INT(n), a, A, MKL_INT(n > 1 ? n : 1), 
+		x, MKL_INT(incx), b, y, MKL_INT(incy));
 }
 template<>
 void _blas_gemv(size_t m, size_t n, f64_t a, const dptr A, const dptr x, int incx, f64_t b, dptr y, size_t incy) {
-	return cblas_dgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasNoTrans, m, n, a, A, (n > 1 ? n : 1), x, incx, b, y, incy);
+	return cblas_dgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasNoTrans, 
+		MKL_INT(m), MKL_INT(n), a, A, MKL_INT(n > 1 ? n : 1), 
+		x, MKL_INT(incx), b, y, MKL_INT(incy));
 }
 template<>
 void _blas_gemtv(size_t m, size_t n, f32_t a, const fptr A, const fptr x, int incx, f32_t b, fptr y, size_t incy) {
-	return cblas_sgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasTrans, m, n, a, A, (m > 1 ? m : 1), x, incx, b, y, incy);
+	return cblas_sgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasTrans, MKL_INT(m), MKL_INT(n), a, A, MKL_INT(m > 1 ? m : 1), x, MKL_INT(incx), b, y, MKL_INT(incy));
 }
 template<>
 void _blas_gemtv(size_t m, size_t n, f64_t a, const dptr A, const dptr x, int incx, f64_t b, dptr y, size_t incy) {
-	return cblas_dgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasTrans, m, n, a, A, (m > 1 ? m : 1), x, incx, b, y, incy);
+	return cblas_dgemv(CBLAS_LAYOUT::CblasRowMajor, CBLAS_TRANSPOSE::CblasTrans, MKL_INT(m), MKL_INT(n), a, A, MKL_INT(m > 1 ? m : 1), x, MKL_INT(incx), b, y, MKL_INT(incy));
 }
 template<>
 void _blas_gemm(int lyt, int trpa, int trpb, size_t m, size_t n, size_t k, f32_t a, const fptr A, size_t lda, const fptr B, size_t ldb, f32_t b, fptr C, size_t ldc) {
-	return cblas_sgemm(CBLAS_LAYOUT(lyt), CBLAS_TRANSPOSE(trpa), CBLAS_TRANSPOSE(trpb), MKL_INT(m), MKL_INT(n), MKL_INT(k), a, A, lda, B, ldb, b, C, ldc);
+	return cblas_sgemm(CBLAS_LAYOUT(lyt), CBLAS_TRANSPOSE(trpa), CBLAS_TRANSPOSE(trpb), MKL_INT(m), MKL_INT(n), MKL_INT(k), a, A, MKL_INT(lda), B, MKL_INT(ldb), b, C, MKL_INT(ldc));
 }
 template<>
 void _blas_gemm(int lyt, int trpa, int trpb, size_t m, size_t n, size_t k, f64_t a, const dptr A, size_t lda, const dptr B, size_t ldb, f64_t b, dptr C, size_t ldc) {
-	return cblas_dgemm(CBLAS_LAYOUT(lyt), CBLAS_TRANSPOSE(trpa), CBLAS_TRANSPOSE(trpb), MKL_INT(m), MKL_INT(n), MKL_INT(k), a, A, lda, B, ldb, b, C, ldc);
+	return cblas_dgemm(CBLAS_LAYOUT(lyt), CBLAS_TRANSPOSE(trpa), CBLAS_TRANSPOSE(trpb), MKL_INT(m), MKL_INT(n), MKL_INT(k), a, A, MKL_INT(lda), B, MKL_INT(ldb), b, C, MKL_INT(ldc));
 }
 _INTERNAL_END
 
