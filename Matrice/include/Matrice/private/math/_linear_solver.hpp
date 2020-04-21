0ff308a21fabab9c952e878ee99d8a5955ba0e54
Zhilong Su <su-zl@seu.edu.cn>
2020-04-21 11:15:54 +0800

    Improve the linear solvers.

--- Matrice/include/Matrice/private/math/_linear_solver.hpp
+++ Matrice/include/Matrice/private/math/_linear_solver.hpp
@@ -58,11 +58,13 @@ public:
 	}
 
 	/**
-	 *\brief Solve AX = B. The solver kernel is dispatched according to the solver operator specified to _Op.
+	 *\brief Solve $\mathbf{AX} = \mathbf{B}$. The solver kernel is dispatched according to the solver operator specified to _Op.
+	   The right-hand term $\mathbf{B}$ is allowed to hold more than one column when the linear kernel _Op is dgelom::spt.
+	 *\param [args...] variadic arguments that may empty or more than one inputs. If args... is empty, the method solves a homogeneous linear system with form of $\mathbf{Ax} = \mathbf{0}$.
 	 */
-	template<class _Rty>
-	MATRICE_GLOBAL_INL decltype(auto) solve(_Rty& b) {
-		return ((_Myderived*)(this))->backward(b);
+	template<class... _Args>
+	MATRICE_GLOBAL_INL decltype(auto)solve(_Args&... args) {
+		return ((_Myderived*)(this))->_Xbackward(args...);
 	}
 
 	/**
@@ -77,13 +79,15 @@ protected:
 	 *\brief Perform coeff matrix decomposition
 	 */
 	MATRICE_GLOBAL_INL void _Forward() {
-		auto _Dptr = static_cast<_Myderived*>(this);
 		using kernel_t = typename _Mytraits::op_type;
+
+		auto _Mdp = static_cast<_Myderived*>(this);
 		if constexpr (is_same_v<kernel_t, void>) {
 			//_Mycoeff is a general square matrix, _Forward does nothing.
 		}
 		if constexpr (is_same_v<kernel_t, svd>) {
-			internal::_Lak_adapter<svd>(view(_Mycoeff), view(_Dptr->s()), view(_Dptr->vt()));
+			internal::_Lak_adapter<svd>(view(_Mycoeff), view(_Mdp->s()),
+				view(_Mdp->vt()));
 		}
 		if constexpr (is_same_v<kernel_t, spt>) {
 			internal::_Lak_adapter<spt>(view(_Mycoeff));
@@ -94,8 +98,9 @@ protected:
 	}
 
 	MATRICE_GLOBAL_INL auto _Inverse() {
-		auto _Dptr = (_Myderived*)(this);
 		using kernel_t = typename _Mytraits::op_type;
+
+		auto _Mdp = (_Myderived*)(this);
 		if constexpr (is_same_v<kernel_t, void>) {
 			//general matrix inverse
 			matrix_type _Ret(_Mycoeff.rows(), _Mycoeff.cols());
@@ -103,14 +108,15 @@ protected:
 			return move(_Ret);
 		}
 		if constexpr (is_same_v<kernel_t, svd>) {
-			Matrix_<value_type, matrix_type::cols_at_compiletime, matrix_type::rows_at_compiletime> _Ret(_Mycoeff.cols(), _Mycoeff.rows());
-			_Ret.cview(0) = _Dptr->s();
-			internal::_Inv_adapter<svd>(view(_Mycoeff), view(_Ret), view(_Dptr->vt()));
+			Matrix_<value_type, matrix_type::cols_at_compiletime, matrix_type::rows_at_compiletime> _Ret(_Mycoeff.cols(), 
+				_Mycoeff.rows());
+			internal::_Inv_adapter<kernel_t>(view(_Mycoeff), view(_Mdp->s()),
+				view(_Mdp->vt()), view(_Ret));
 			return move(_Ret);
 		}
 		if constexpr (is_same_v<kernel_t, spt>) {
 			matrix_type _Ret(_Mycoeff.rows(), _Mycoeff.cols());
-			internal::_Inv_adapter<spt>(view(_Mycoeff), view(_Ret));
+			internal::_Inv_adapter<kernel_t>(view(_Mycoeff), view(_Ret));
 			return move(_Ret);
 		}
 	}
@@ -157,15 +163,31 @@ private:
 // \note This solver is only for symmetric positive definite systems. 
 MATRICE_MAKE_LINEAR_SOLVER_SPEC_BEGIN(spt)
 public:
-	// \note The input coeff will be overwritten by $L$
+	/**
+	 *\note The input coeff will be overwritten by $L$
+	 */
 	_Linear_solver(matrix_type& coeff) noexcept
 		: _Mybase(coeff) {
 		_Mybase::_Forward();
 	}
 
-	// \brief Perform back substitution to solve ${AX = B}$, where B allowed to have multi-cols and will be overwritten by X.
+	/**
+	 *  \brief Returns the determinant of the given coeff matrix.
+	 */
+	MATRICE_GLOBAL_INL auto(det)()const noexcept {
+		decltype(auto) _L = _Mybase::_Mycoeff;
+		auto _Ret = one<value_type>;
+		for (auto _Idx = 0; _Idx < _L.rows(); ++_Idx)
+			_Ret *= _L[_Idx][_Idx];
+		return sqr(_Ret);
+	}
+
+	/**
+	 * \brief Perform back substitution to solve $\mathbf{AX} = \mathbf{B}$, where $\mathbf{B}$ allowed to have multi-cols and will be overwritten by $\mathbf{X}$.
+	 */
 	template<class _Rty>
-	MATRICE_GLOBAL_INL _Rty& backward(_Rty& B) noexcept {
+	[[noextcall]]
+	MATRICE_GLOBAL_INL _Rty& _Xbackward(_Rty& B)const noexcept {
 		decltype(auto) _L = _Mybase::_Mycoeff;
 #ifdef MATRICE_DEBUG
 		DGELOM_CHECK(_L.rows() == B.rows(),
@@ -174,63 +196,87 @@ public:
 		internal::_Bwd_adapter<_Myop>(view(_L), view(B));
 		return (B);
 	}
-
-	// \brief Returns the determinant of the given coeff matrix.
-	MATRICE_GLOBAL_INL auto (det)() const noexcept {
-		decltype(auto) _L = _Mybase::_Mycoeff;
-		auto _Ret = one<value_type>;
-		for (auto _Idx = 0; _Idx < _L.rows(); ++_Idx)
-			_Ret *= _L[_Idx][_Idx];
-		return sqr(_Ret);
-	}
 MATRICE_MAKE_LINEAR_SOLVER_SPEC_END(spt)
 
 // \brief CLASS TEMPLATE for SVD based linear solver
 // \param <_Mty> Matrice compatible matrix type
 MATRICE_MAKE_LINEAR_SOLVER_SPEC_BEGIN(svd)
 public:
-	// \note: the input coeff will be overwritten by U
+	/**
+	 *  \note: the input coeff will be overwritten by $\mathbf{U}$
+	 */
 	_Linear_solver(matrix_type& coeff) 
 		: _Mybase(coeff), 
-		_Mys(coeff.cols()), 
-		_Myvt(coeff.cols(), coeff.cols()) {
+		_Mys(coeff.cols()), _Myvt(coeff.cols(), coeff.cols()) {
+		_Mybase::_Myeps *= value_type(0.5) * sqrt<value_type>(coeff.size());
 		_Mybase::_Forward();
 	}
 
-	MATRICE_GLOBAL_INL decltype(auto) u() const noexcept {
+	MATRICE_GLOBAL_INL decltype(auto)u()const noexcept {
 		return (_Mybase::_Mycoeff);
 	}
-	MATRICE_GLOBAL_INL decltype(auto) u() noexcept {
+	MATRICE_GLOBAL_INL decltype(auto)u()noexcept {
 		return (_Mybase::_Mycoeff);
 	}
-	MATRICE_GLOBAL_INL decltype(auto) s() const noexcept {
+	MATRICE_GLOBAL_INL decltype(auto)s()const noexcept {
 		return (_Mys);
 	}
-	MATRICE_GLOBAL_INL decltype(auto) s() noexcept {
+	MATRICE_GLOBAL_INL decltype(auto)s()noexcept {
 		return (_Mys);
 	}
-	MATRICE_GLOBAL_INL decltype(auto) vt() const noexcept {
+	MATRICE_GLOBAL_INL decltype(auto)vt()const noexcept {
 		return (_Myvt);
 	}
-	MATRICE_GLOBAL_INL decltype(auto) vt() noexcept {
+	MATRICE_GLOBAL_INL decltype(auto)vt()noexcept {
 		return (_Myvt);
-	}
-
-	template<typename _Rty>
-	MATRICE_GLOBAL_INL _Rty& backward(_Rty& b) noexcept {
-
 	}
 
 	/**
+	 *\brief Return the rank of the given source matrix.
+	 */
+	MATRICE_GLOBAL_INL size_t rank()const noexcept {
+		const auto _Thresh = _Mys(0) * _Mybase::_Myeps;
+		auto _Ret = size_t(0);
+		for (const auto& _Val : _Mys) {
+			(_Val > _Thresh) ? ++_Ret : _Ret;
+		}
+		return _Ret;
+	}
+	/**
+	 *\brief Return the nullity of the given source matrix.
+	 */
+	MATRICE_GLOBAL_INL size_t nullity()const noexcept {
+		const auto _Thresh = _Mys(0) * _Mybase::_Myeps;
+		auto _Ret = size_t(0);
+		for (const auto& _Val : _Mys) {
+			(_Val <= _Thresh) ? ++_Ret : _Ret;
+		}
+		return _Ret;
+	}
+
+	/**
+     * \brief Solve the system $\mathbf{A}\mathbf{x} = \mathbf{b}$ with the pre-computed coeff. matrix, and $\mathbf{b}$ is overwritten by the solution $\mathbf{x}$.
+     */
+	template<typename _Bty> 
+	[[noextcall]]
+	MATRICE_GLOBAL_INL auto _Xbackward(_Bty& b) noexcept {
+#ifdef MATRICE_DEBUG
+		DGELOM_CHECK(b.size() == _Mybase::_Mycoeff.rows(), "Bad size in _Linear_solver<_Mty, svd>::backward(...).");
+#endif
+		decltype(_Mys) x;
+		internal::_Bwd_adapter<_Myop>(view(_Mybase::_Mycoeff), view(_Mys), view(_Myvt), view(b), view(x));
+		return forward<decltype(x)>(x);
+	}
+	/**
 	 *\brief Solve $\mathbf{A}\mathbf{x} = \mathbf{0}$, but the method returns the view of the solution rather than a vector.
 	 */
-	MATRICE_GLOBAL_INL auto solve() noexcept {
+	[[noextcall]]
+	MATRICE_GLOBAL_INL auto _Xbackward()const noexcept {
 		return _Myvt.rview(size_t(_Myvt.rows()) - 1);
 	}
 private:
 	Matrix_<value_type, _Mty::cols_at_compiletime, 1> _Mys;
 	Matrix_<value_type, _Mty::cols_at_compiletime> _Myvt;
-
 MATRICE_MAKE_LINEAR_SOLVER_SPEC_END(svd);
 
 template<class _Mty, typename _Op> 
