e079276e06cee076c7f135078d269900a5b81072
Zhilong Su <su-zl@seu.edu.cn>
2020-04-23 20:01:43 +0800

    Make ::ref() as a deleted method.

--- Matrice/include/Matrice/core/matrix.h
+++ Matrice/include/Matrice/core/matrix.h
@@ -1,6 +1,6 @@
-/**************************************************************************
+/*********************************************************************
 This file is part of Matrice, an effcient and elegant C++ library.
-Copyright(C) 2018-2019, Zhilong(Dgelom) Su, all rights reserved.
+Copyright(C) 2018-2020, Zhilong(Dgelom) Su, all rights reserved.
 
 This program is free software : you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.If not, see <http://www.gnu.org/licenses/>.
-**************************************************************************/
+**********************************************************************/
 #pragma once
 #include <array>
 #include "private/_plain_base.hpp"
@@ -109,6 +109,8 @@ public:
 	MATRICE_GLOBAL_FINL operator Matrix_<value_t, ::dynamic>() noexcept {
 		return Matrix_<value_t, ::dynamic>(rows(), cols(), _Mybase::m_data); 
 	}
+
+	template<typename _Arg> _Myt& ref(_Arg& _) noexcept = delete;
 };
 
 template<typename _Ty, size_t Rows>
