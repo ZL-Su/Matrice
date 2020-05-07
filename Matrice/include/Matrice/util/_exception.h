ff297b3de9aede1f71103fdb1fa0831456afa3c1
Zhilong Su <su-zl@seu.edu.cn>
2020-05-07 14:26:26 +0800

    Add message() method in _Exception_wrapper::error.

--- Matrice/include/Matrice/util/_exception.h
+++ Matrice/include/Matrice/util/_exception.h
@@ -1,6 +1,20 @@
 /*********************************************************************
-	  About License Agreement of this file, see "../lic/license.h"
-*********************************************************************/
+This file is part of Matrice, an effcient and elegant C++ library.
+Copyright(C) 2018-2020, Zhilong(Dgelom) Su, all rights reserved.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
 #pragma once
 #include <functional>
 #include <exception>
@@ -46,6 +60,16 @@ struct _Exception_wrapper
 		 */
 		MATRICE_HOST_INL loc_type location() const noexcept {
 			return (_Myloc);
+		}
+
+		/**
+		 * \return exception message 
+		 */
+		MATRICE_HOST_INL msg_type message() const noexcept {
+			return std::string(" [MSG]: ") + this->what() 
+				+ "\n [Func]: " + _Myloc._func 
+				+ "\n [Line]: " + std::to_string(_Myloc._line) 
+				+ "\n [File]: " + _Myloc._file;
 		}
 
 	private:
