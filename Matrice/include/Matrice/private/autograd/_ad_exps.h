14932e4fb58396e369ea8fe71b4222a0f5d5eb2d
Zhilong Su <su-zl@seu.edu.cn>
2020-06-13 19:41:00 +0800

    Add single variable auto-diff expressions.

--- Matrice/include/Matrice/private/autograd/_ad_exps.h
+++ Matrice/include/Matrice/private/autograd/_ad_exps.h
@@ -1,39 +1,311 @@
 /*********************************************************************
-	  About License Agreement of this file, see "../lic/license.h"
-*********************************************************************/
+This file is part of Matrice, an effcient and elegant C++ library.
+Copyright(C) 2018-2020, Zhilong(Dgelom) Su, all rights reserved.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
 #pragma once
-#include "_plain_exp.hpp"
+#include "_ad_utils.h"
 
-///<brief> construct computational expression system </brief>
-///<method> exp::forward(): for expression evaluation </method>
-///<method> exp::backward(): for derivatives computation based on chain rule </method>
+DGE_MATRICE_BEGIN
+namespace ade {
+template<class _Derived, 
+	typename _Ty = typename _Derived::value_type>
+class auto_diff_exp {
+	using _Myt = auto_diff_exp;
 
-DGE_MATRICE_BEGIN namespace exp {
-_DETAIL_BEGIN
+protected:
+	struct _Myval_tag {};
+	struct _Mydiff_tag {};
 
-template<typename _Derived> class _Expression_base
-{
-	using _Myt = _Expression_base;
-	using _Mydt = _Derived;
 public:
-	_Expression_base() {};
-	~_Expression_base() {};
+	using derived_type = _Derived;
+	using value_type = _Ty;
 
-	/**
-	 * \return value of an expresstion
-	 */
-	MATRICE_GLOBAL_INL auto forward() const {
-		return (static_cast<const _Mydt*>(this)->operator());
+	/// <summary>
+	/// Evaluate the value of this expression.
+	/// </summary>
+	MATRICE_GLOBAL_FINL constexpr value_type operator()()const noexcept {
+		return this->value();
 	}
-	/**
-	 * \return gradient of an expresstion
-	 */
-	MATRICE_GLOBAL_INL auto backward() const {
+	MATRICE_GLOBAL_FINL constexpr value_type value()const noexcept {
+		return _Dptr()->_Eval<_Myval_tag>();
+	}
+	
+	/// <summary>
+	/// Evaluate the derivative of this expression
+	/// </summary>
+	MATRICE_GLOBAL_FINL constexpr const value_type deriv()const noexcept {
+		return _Dptr()->_Eval<_Mydiff_tag>();
+	}
+	MATRICE_GLOBAL_FINL constexpr value_type deriv() noexcept {
+		return _Dptr()->_Eval<_Mydiff_tag>();
+	}
 
+private:
+	MATRICE_GLOBAL_FINL decltype(auto) _Dptr()const noexcept {
+		return static_cast<const derived_type*>(this);
+	}
+	MATRICE_GLOBAL_FINL decltype(auto) _Dptr() noexcept {
+		return static_cast<derived_type*>(this);
+	}
+};
+template<class T>
+struct is_autodiff_exp<auto_diff_exp<T>> : std::true_type {};
+
+template<typename _Ty>
+class auto_diff_variable : public auto_diff_exp<auto_diff_variable<_Ty>,_Ty> {
+	using _Mybase = auto_diff_exp<auto_diff_variable<_Ty>, _Ty>;
+public:
+	using value_type = _Ty;
+	MATRICE_GLOBAL_INL auto_diff_variable() noexcept = default;
+	MATRICE_GLOBAL_INL auto_diff_variable(value_type x) noexcept
+		:_Myval(x) {}
+
+	template<class _Ety>
+	MATRICE_GLOBAL_FINL constexpr auto _Eval() const noexcept {
+		if constexpr (is_same_v<_Ety, typename _Mybase::_Myval_tag>)
+			return _Myval;
+		if constexpr (is_same_v<_Ety, typename _Mybase::_Mydiff_tag>)
+			return value_type(1);
+		static_assert(is_any_of_v<_Ety, typename _Mybase::_Myval_tag, typename _Mybase::_Mydiff_tag>, "Invalid type parameter in ::_Eval<_Ety>.");
 	}
 private:
-
+	value_type _Myval = 0;
 };
+template<typename _Ty>
+struct is_autodiff_exp<auto_diff_variable<_Ty>> : std::true_type {};
 
-_DETAIL_END
-} DGE_MATRICE_END
+/// <summary>
+/// Auto differential expression for binary addition.
+/// </summary>
+/// <typeparam name="_Lty">Type for left hand side operand</typeparam>
+/// <typeparam name="_Rty">Type for right hand side operand</typeparam>
+template<class _Lty, class _Rty>
+class diff_add_exp {
+public:
+	using value_type = std::common_type_t<typename _Lty::value_type, typename _Rty::value_type>;
+
+	MATRICE_GLOBAL_INL diff_add_exp(const _Lty& x, const _Rty& y)noexcept
+		:m_varx(x), m_vary(y) {
+	}
+
+	MATRICE_GLOBAL_FINL auto value()const noexcept {
+		return m_varx.value() + m_vary.value();
+	}
+
+	MATRICE_GLOBAL_FINL auto deriv() const noexcept {
+		return m_varx.deriv() + m_vary.deriv();
+	}
+
+private:
+	const _Lty& m_varx;
+	const _Rty& m_vary;
+};
+template<class _Lty>
+class diff_add_exp<_Lty, typename _Lty::value_type> {
+public:
+	using value_type = typename _Lty::value_type;
+
+	MATRICE_GLOBAL_INL diff_add_exp(const _Lty& x, const value_type s)noexcept
+		:m_varx(x), m_const(s) {
+	}
+
+	MATRICE_GLOBAL_FINL auto value() const noexcept {
+		return m_varx.value() + m_const;
+	}
+	MATRICE_GLOBAL_FINL auto deriv()const noexcept {
+		return m_varx.deriv();
+	}
+private:
+	const _Lty& m_varx;
+	value_type m_const;
+};
+template<class _Rty>
+struct diff_add_exp<typename _Rty::value_type, _Rty>
+	: public diff_add_exp<_Rty, typename _Rty::value_type> {
+	using _Mybase = diff_add_exp<_Rty, typename _Rty::value_type>;
+public:
+	using typename _Mybase::value_type;
+
+	MATRICE_GLOBAL_INL diff_add_exp(const value_type s, const _Rty& x)noexcept
+		:_Mybase(x, s) {
+	}
+};
+template<typename T, typename U>
+struct is_autodiff_exp<diff_add_exp<T, U>> : std::true_type {};
+
+template<class _Lty, class _Rty>
+class diff_sub_exp {
+public:
+	using value_type = std::common_type_t<typename _Lty::value_type, typename _Rty::value_type>;
+
+	MATRICE_GLOBAL_INL diff_sub_exp(const _Lty& x, const _Rty& y)noexcept
+		:m_varx(x), m_vary(y) {
+	}
+
+	MATRICE_GLOBAL_FINL auto value()const noexcept {
+		return m_varx.value() - m_vary.value();
+	}
+
+	MATRICE_GLOBAL_FINL auto deriv() const noexcept {
+		return m_varx.deriv() - m_vary.deriv();
+	}
+
+private:
+	const _Lty& m_varx;
+	const _Rty& m_vary;
+};
+template<class _Lty>
+class diff_sub_exp<_Lty, typename _Lty::value_type> {
+public:
+	using value_type = typename _Lty::value_type;
+
+	MATRICE_GLOBAL_INL diff_sub_exp(const _Lty& x, const value_type s)noexcept
+		:m_varx(x), m_const(s) {
+	}
+
+	MATRICE_GLOBAL_FINL auto value() const noexcept {
+		return m_varx.value() - m_const;
+	}
+	MATRICE_GLOBAL_FINL auto deriv()const noexcept {
+		return m_varx.deriv();
+	}
+private:
+	const _Lty& m_varx;
+	value_type m_const;
+};
+template<class _Rty>
+struct diff_sub_exp<typename _Rty::value_type, _Rty>
+	: public diff_sub_exp<_Rty, typename _Rty::value_type> {
+	using _Mybase = diff_sub_exp<_Rty, typename _Rty::value_type>;
+public:
+	using typename _Mybase::value_type;
+
+	MATRICE_GLOBAL_INL diff_sub_exp(const value_type s, const _Rty& x)noexcept
+		:_Mybase(x, s) {
+	}
+};
+template<typename T, typename U>
+struct is_autodiff_exp<diff_sub_exp<T, U>> : std::true_type {};
+
+template<class _Lty, class _Rty>
+struct diff_mul_exp {
+public:
+	using value_type = std::common_type_t<typename _Lty::value_type, typename _Rty::value_type>;
+
+	MATRICE_GLOBAL_INL diff_mul_exp(const _Lty& x, const _Rty& y)noexcept
+		:m_varx(x), m_vary(y) {
+	}
+
+	MATRICE_GLOBAL_FINL auto value()const noexcept {
+		return m_varx.value() * m_vary.value();
+	}
+
+	MATRICE_GLOBAL_FINL auto deriv() const noexcept {
+		return m_varx.deriv() * m_vary.value() + m_varx.value() * m_vary.deriv();
+	}
+
+private:
+	const _Lty& m_varx;
+	const _Rty& m_vary;
+};
+template<class _Lty>
+struct diff_mul_exp<_Lty, typename _Lty::value_type> {
+	using value_type = typename _Lty::value_type;
+	MATRICE_GLOBAL_FINL diff_mul_exp(const _Lty& x, const value_type s)noexcept
+		:m_varx(x), m_const(s) {
+	}
+
+	MATRICE_GLOBAL_FINL auto value() const noexcept {
+		return m_varx.value() * m_const;
+	}
+	MATRICE_GLOBAL_FINL auto deriv()const noexcept {
+		return m_varx.deriv() * m_const;
+	}
+private:
+	const _Lty& m_varx;
+	value_type m_const;
+};
+template<class _Rty>
+struct diff_mul_exp<typename _Rty::value_type, _Rty>
+	: public diff_mul_exp<_Rty, typename _Rty::value_type> {
+	using _Mybase = diff_mul_exp<_Rty, typename _Rty::value_type>;
+public:
+	using typename _Mybase::value_type;
+	MATRICE_GLOBAL_INL diff_mul_exp(const value_type s, const _Rty& x)noexcept
+		:_Mybase(x, s) {
+	}
+};
+template<typename T, typename U>
+struct is_autodiff_exp<diff_mul_exp<T, U>> : std::true_type {};
+
+#define DGELOM_MAKE_UNARY_AUTODIFF_EXP(NAME, VEXP, DEXP) \
+	template<class _Lty> class diff_##NAME##_exp { \
+	public: \
+		using value_type = typename _Lty::value_type; \
+		MATRICE_GLOBAL_INL diff_##NAME##_exp(const _Lty& x) noexcept \
+			:m_varx(x) {} \
+		MATRICE_GLOBAL_FINL auto value() const noexcept { \
+			VEXP; \
+		} \
+		MATRICE_GLOBAL_FINL auto deriv()const noexcept { \
+			DEXP; \
+		} \
+	private: \
+		const _Lty& m_varx; \
+	}; \
+	template<typename T> \
+	struct is_autodiff_exp<diff_##NAME##_exp<T>> : std::true_type {}; \
+	template<class _Lty, MATRICE_ENABLE_IF(is_autodiff_exp_v<_Lty>)> \
+	MATRICE_GLOBAL_FINL auto NAME(const _Lty& x) noexcept { \
+		return diff_##NAME##_exp{ x }; \
+	}
+
+DGELOM_MAKE_UNARY_AUTODIFF_EXP(sin,
+	return std::sin(m_varx.value()),
+	return value() * m_varx.deriv()
+);
+DGELOM_MAKE_UNARY_AUTODIFF_EXP(cos,
+	return std::cos(m_varx.value()),
+	return -std::sin(m_varx.value()) * m_varx.deriv()
+);
+DGELOM_MAKE_UNARY_AUTODIFF_EXP(exp,
+	return std::exp(m_varx.value()),
+	return value() * m_varx.deriv()
+);
+DGELOM_MAKE_UNARY_AUTODIFF_EXP(cube,
+	return std::pow(m_varx.value(), 3),
+	return 3 * m_varx.value() * m_varx.value() * m_varx.deriv()
+);
+DGELOM_MAKE_UNARY_AUTODIFF_EXP(sq,
+	return m_varx.value() * m_varx.value(),
+	return 2 * m_varx.value() * m_varx.deriv()
+);
+
+template<class _Lty, class _Rty>
+MATRICE_GLOBAL_FINL auto operator+(const _Lty& l, const _Rty& r)noexcept {
+	return diff_add_exp{ l, r };
+}
+template<class _Lty, class _Rty>
+MATRICE_GLOBAL_FINL auto operator-(const _Lty& l, const _Rty& r)noexcept {
+	return diff_sub_exp{ l, r };
+}
+template<class _Lty, class _Rty>
+MATRICE_GLOBAL_FINL auto operator*(const _Lty& l, const _Rty& r)noexcept {
+	return diff_mul_exp{ l, r };
+}
+#undef DGELOM_MAKE_UNARY_AUTODIFF_EXP
+}
+DGE_MATRICE_END
