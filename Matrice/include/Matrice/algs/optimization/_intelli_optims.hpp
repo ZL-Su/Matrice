17a5a6671c63a7592e7e0ad4ecd6c2a3c6891a2e
Zhilong Su <su-zl@seu.edu.cn>
2020-06-17 16:12:40 +0800

    Remove concept and add constant and linease decrease inertia weights.

--- Matrice/include/Matrice/algs/optimization/_intelli_optims.hpp
+++ Matrice/include/Matrice/algs/optimization/_intelli_optims.hpp
@@ -19,64 +19,89 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #include <core/matrix.h>
 #include "../similarity.h"
 
-MATRICE_ALGS_BEGIN
-template<typename T>
-concept real_type = requires {is_floating_point_v<T>; };
-
-/// <summary>
-/// ZNCC fitness function
-/// </summary>
-/// <typeparam name="T"></typeparam>
-template<typename T> struct zncc_fitness {
-	size_t m_size = 31;
-	T* m_ref = nullptr;
-
-	/// <summary>
-	/// Evaluate the fitness value.
-	/// </summary>
-	/// <param name="target">Subset to be matched against</param>
-	/// <returns>Fitness value</returns>
-	auto operator() (T* const target) const noexcept {
-		Metric_<metric_fn::ZNCC, T> metric(m_ref, m_size);
-		return metric.eval(target);
-	}
-};
-
+MATRICE_ALG_BEGIN(pso)
 _DETAIL_BEGIN
+
+struct _Constant_tag {};
+struct _Linear_decrease_tag {};
+
+template<typename _Ty, class _Tag> class _Inertia_weight {
+	static_assert(true, "Unsupported _Tag in _Inertia_weight<_Tag>");
+};
 
 /// <summary>
 /// Particle class for PSO algorithm
 /// </summary>
 /// <typeparam name="_Ty"> float or double </typeparam>
-template<typename _Ty> 
-requires real_type<_Ty> class _Particle 
+template<typename _Ty, size_t _Dim> class _Particle{};
+template<typename _Ty>
+class _Particle<_Ty, 2> 
 {
 public:
 	using value_type = _Ty;
 	using pos_type = Vec2_<value_type>;
 	struct options {
-		value_type max_x = 0, min_x = 0;
-		value_type max_y = 0, min_y = 0;
-		value_type max_v = 0, min_v = 0;
+		value_type max_x = 0, min_x = 0; //max and min x-position
+		value_type max_y = 0, min_y = 0; //max and min y-position
+		value_type max_v = 0, min_v = 0; //max and min velocity
 		value_type c_1 = 0, c_2 = 0;
 	};
 
-	explicit _Particle(const pos_type& pos) noexcept
-		:_Mypos(pos) {
+	explicit _Particle(const pos_type& pos, const options& opt) noexcept
+		:_Mypos(pos), _Myopt(opt) {
+	}
+
+	template<class _Fty>
+	auto eval_fval(_Fty&& fn) const {
+		return _Myfval = fn(_Mypos);
 	}
 
 
 private:
-	options _Myopt;
-	value_type _Myfs;
+	const options& _Myopt;
 	pos_type _Mypos, _Mybest;
-	pos_type& _Myglobal;
+	mutable value_type _Myfval;
+	mutable pos_type& _Mygbest;
 
 };
 
-template<class _Type>
-MATRICE_GLOBAL_INL auto _Optimize(const _Type& t) noexcept {
+template<class _Type> class _Optimizer {
 
-}
+};
+
+template<typename _Ty> 
+class _Inertia_weight<_Ty, _Constant_tag> {
+public:
+	using value_type = _Ty;
+	MATRICE_HOST_INL _Inertia_weight() noexcept = default;
+	MATRICE_HOST_INL _Inertia_weight(const value_type w) noexcept 
+		: _Myval(w) {
+	}
+	
+	MATRICE_HOST_FINL auto operator()(size_t, size_t) const noexcept {
+		return _Myval;
+	}
+
+private:
+	value_type _Myval{ 1 };
+};
+template<typename _Ty> 
+class _Inertia_weight<_Ty, _Linear_decrease_tag> {
+public:
+	using value_type = _Ty;
+	MATRICE_HOST_INL _Inertia_weight() noexcept = default;
+	MATRICE_HOST_INL _Inertia_weight(const value_type min, const value_type max)noexcept 
+		: _Mymin(min), _Mymax(max) {
+	}
+	
+	MATRICE_HOST_FINL auto operator()(size_t it, size_t max_it) const noexcept {
+		return _Mymin + (_Mymax - _Mymin) * it / value_type(max_it);
+	}
+
+private:
+	value_type _Mymin{ 0.4 };
+	value_type _Mymax{ 0.9 };
+};
+
 _DETAIL_END
-MATRICE_ALGS_END
+MATRICE_ALG_END(pso)
