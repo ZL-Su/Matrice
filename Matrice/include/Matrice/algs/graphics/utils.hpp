a36f0f6f1bb34d05ae22bbf5b3dabd29ade75a98
Zhilong Su <su-zl@seu.edu.cn>
2021-01-10 15:14:28 +0800

    Update dgelom::linspace<_Ty>.

--- Matrice/include/Matrice/algs/graphics/utils.hpp
+++ Matrice/include/Matrice/algs/graphics/utils.hpp
@@ -25,6 +25,8 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 DGE_MATRICE_BEGIN
+template<typename _Ty> class linspace;
+
 _DETAIL_BEGIN
 template<typename _Pty = Vec3_<float>>
 class _Meshgrid_based
@@ -38,7 +40,7 @@ public:
 	_Meshgrid_based(const ptlist_type& _Pts)
 		:_Mypts(std::make_shared(_Pts)) {}
 
-	const decltype(auto) mesh_size() const {
+	decltype(auto) mesh_size() const {
 		return (_Mymesz);
 	}
 	decltype(auto) mesh_size() {
@@ -47,19 +49,19 @@ public:
 
 private:
 	MATRICE_HOST_INL void _Find_bound() {
-		const auto _Minx = std::min(_Mypts.begin(), _Mypts, end(), 
+		const auto _Minx = std::min(_Mypts.begin(), _Mypts.end(), 
 			[](const auto& _Left, const auto& _Right) {
 			return (_Left.x < _Right.x);
 		}).x;
-		const auto _Miny = std::min(_Mypts.begin(), _Mypts, end(),
+		const auto _Miny = std::min(_Mypts.begin(), _Mypts.end(),
 			[](const auto& _Left, const auto& _Right) {
 			return (_Left.y < _Right.y);
 		}).y;
-		const auto _Maxx = std::max(_Mypts.begin(), _Mypts, end(),
+		const auto _Maxx = std::max(_Mypts.begin(), _Mypts.end(),
 			[](const auto& _Left, const auto& _Right) {
 			return (_Left.x > _Right.x);
 		}).x;
-		const auto _Maxy = std::max(_Mypts.begin(), _Mypts, end(),
+		const auto _Maxy = std::max(_Mypts.begin(), _Mypts.end(),
 			[](const auto& _Left, const auto& _Right) {
 			return (_Left.y > _Right.y);
 		}).y;
@@ -71,20 +73,27 @@ private:
 };
 _DETAIL_END
 
-namespace graph {
 /**
  *\brief <class> linspace<_Ty> </class>
  *\typen <_Ty> a scalar type
 */
 template<typename _Ty = float> class linspace {
 	static_assert(is_scalar_v<_Ty>, "_Ty in linspace<> must be a scalar.");
+	using _Myt = linspace<_Ty>;
 public:
 	using value_type = _Ty;
+	using row_vector_type = Matrix_<value_type, 1, ::dynamic>;
 
-	template<typename _Vt = size_t> MATRICE_GLOBAL_INL
-		linspace(value_type begin, value_type end, _Vt span) noexcept
-		: m_begin(begin), m_end(end), m_span(static_cast<value_type>(span)),
-		m_step(safe_div(sub(m_end, m_begin), m_span)) {
+	/// <summary>
+	/// Ctor of class linspace
+	/// </summary>
+	/// <param name="'begin'"> The staring value of the sequence </param>
+	/// <param name="'end'"> The ending value of the sequence </param>
+	/// <param name="'num'"> Number of samples to generate. Default is 51.</param>
+	linspace(value_type begin, value_type end, size_t num = 51) noexcept
+		: m_begin(begin), m_end(end) {
+		m_num = num;
+		m_step = num == 1 ? sub(m_end, m_begin) : safe_div(sub(m_end, m_begin), num - 1);
 	}
 
 	MATRICE_GLOBAL_INL const value_type& begin() const noexcept {
@@ -112,11 +121,35 @@ public:
 	MATRICE_GLOBAL_INL operator bool() const noexcept {
 		return (m_current <= m_end);
 	}
+	/// <summary>
+	/// \brief Return evenly spaced numbers over the specified interval [begin, end].
+	/// If paramerter 'endpoint' is false, then the end point of the interval is excluded.
+	/// </summary>
+	MATRICE_GLOBAL_INL row_vector_type operator()(bool endpoint = true) const {
+		row_vector_type _Ret(endpoint ? m_num : m_num - 1);
+		for (auto i = 0; i < _Ret.size(); ++i)
+			_Ret(i) = i * m_step;
+		return _Ret;
+	}
+
+	/// <summary>
+	/// \brief Return evenly spaced numbers over the specified interval [start, stop].
+	/// </summary>
+	/// <param name="'start'"> The staring value of the sequence. </param>
+	/// <param name="'stop'"> The ending value of the sequence. </param>
+	/// <param name="'num'"> Number of samples to generate. Default is 51. </param>
+	/// <param name="'endpoint'"> If True, stop is the last sample. Otherwise, it is not included. Default is True. </param>
+	/// <returns> A row vector holds the generated number sequence. </returns>
+	static MATRICE_GLOBAL_INL row_vector_type _(_Ty start, _Ty stop, size_t num = 51, bool endpoint = true) {
+		_Myt linspace(start, stop, num);
+		return linspace(endpoint);
+	}
+
 private:
 	value_type m_begin = 0, m_end = 1;
-	value_type m_span, m_step = 1, m_current = m_begin;
+	value_type m_step = 1, m_current = m_begin;
+	size_t m_num;
 };
-}
 
 DGE_MATRICE_END
 
