d391f351e5cf6728b7ddd232e5e964cdd1a7784d
Zhilong (Dgelom) Su <su-zl@seu.edu.cn>
2021-03-02 15:17:44 +0800

    Update license.

--- Matrice/include/Matrice/algs/interpolation.h
+++ Matrice/include/Matrice/algs/interpolation.h
@@ -1,6 +1,6 @@
 /*********************************************************************
 This file is part of Matrice, an effcient and elegant C++ library.
-Copyright(C) 2018-2019, Zhilong(Dgelom) Su, all rights reserved.
+Copyright(C) 2018-2021, Zhilong(Dgelom) Su, all rights reserved.
 
 This program is free software : you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
