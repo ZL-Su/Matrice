3d7343ceb19fd4ce1fa8322eb094649ebfc9e403
Zhilong Su <su-zl@seu.edu.cn>
2021-06-18 09:37:04 +0800

    Updates.
    1. add function inplace_swap;
    2. add function reverse_array;
    3. add function B2U.

--- Matrice/include/Matrice/arch/sys/info_rep.h
+++ Matrice/include/Matrice/arch/sys/info_rep.h
@@ -36,5 +36,40 @@ template<typename _Ty>
 MATRICE_HOST_INL void byte_rep(_Ty _Val) noexcept {
 	_Show_bytes(byte_pointer(&_Val), sizeof(_Ty));
 }
+
+/// <summary>
+/// \brief Swap to the values stored at the locations denoted by pointers x and y.
+/// </summary>
+template<typename _Ty>
+MATRICE_HOST_INL void inplace_swap(_Ty* _Left, _Ty* _Right) noexcept {
+	*_Right = *_Left ^ *_Right;
+	*_Left = *_Left ^ *_Right;
+	*_Right = *_Left ^ *_Right;
+}
+
+/// <summary>
+/// \brief Reverse element order in a given array '_Array'.
+/// </summary>
+template<typename _Ty, size_t _N>
+MATRICE_HOST_INL void reverse_array(_Ty(&_Array)[_N]) noexcept {
+	auto _First = 0ull, _Last = _N - 1;
+	for (; _First < _Last; ++_First, --_Last) {
+		inplace_swap(&_Array[_First], &_Array[_Last]);
+	}
+}
+
+/// <summary>
+/// \brief Convert arrays of zeros and ones of length _W to nonnegative integers.
+/// </summary>
+/// <param name="_Bits">: Array of zeros and ones.</param>
+/// <returns> A nonnegative integer. </returns>
+template<size_t _W>
+MATRICE_HOST_INL uint32_t B2U(bool(&_Bits)[_W]) noexcept {
+	auto _Ret = uint32_t(0);
+	for (auto _Idx = 0; _Idx < _W; ++_Idx) {
+		_Ret += _Bits[_Idx] * pow(2, _W - 1 - _Idx);
+	}
+	return _Ret;
+}
 }
 DGE_MATRICE_END
