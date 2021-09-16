2b0cff2ef363f6393f3dfbf23aa8cdedf76eb951
Zhilong Su <su-zl@seu.edu.cn>
2021-09-16 21:23:12 +0800

    Update comment of MATRICE_NOALIAS.

--- Matrice/include/Matrice/util/_macros.h
+++ Matrice/include/Matrice/util/_macros.h
@@ -161,7 +161,7 @@ typename = std::enable_if_t<_COND>
 #ifndef MATRICE_NOALIAS
 // MATRICE_NOALIAS attribute tells the compiler optimizer that pointers 
 // going into the modified algorithms won't be stored beyond the lifetime 
-// of the function, and that the function will only reference arrays 
-// denoted by those pointers.
+// of the function, and that the function will only reference arrays denoted 
+// by those pointers. Functions using MATRICE_NOALIAS must usually return void.
 #define MATRICE_NOALIAS __declspec(noalias)
 #endif
