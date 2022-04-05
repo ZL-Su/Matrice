db944869bde69b6950a9b177f3b479cbb52c17bb
ZL-Su <zhilong8845@shu.edu.cn>
2022-04-05 20:50:35 +0800

    Update _input_layer.hpp

--- Matrice/include/Matrice/algs/dnn/layer/_input_layer.hpp
+++ Matrice/include/Matrice/algs/dnn/layer/_input_layer.hpp
@@ -18,42 +18,83 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #include "_layer_base.hpp"
 
-DGE_MATRICE_BEGIN
-namespace dnn {
+MATRICE_NAMESPACE_BEGIN(dnn)
+namespace detail {
 
-	template<typename _Ity> class _Input_layer {
-		static_assert(sizeof(_Ity) != sizeof(_Ity),
-			"Unsupported type given to _Input_layer<>, which only supports dgelom::Matrix_, dgelom::Tensor or array of dgelom::Matrix_.");
-	};
+template<typename _Ity> class _Input_layer {
+	static_assert(sizeof(_Ity) != sizeof(_Ity),
+		"Unsupported type in _Input_layer<_Ity>, \
+		only supports dgelom::Matrix_, \
+		dgelom::Tensor or array of dgelom::Matrix_.");
+};
 
-	template<typename _Ty, int _M, int _N>
-	class _Input_layer<Matrix_<_Ty, _M, _N>> {
-		using _Myt = _Input_layer;
-	public:
-		using input_type = Matrix_<_Ty, _M, _N>;
-		using category = typename _Layer_tag::input;
+template<typename _Ty, int _M, int _N>
+class _Input_layer<Matrix_<_Ty, _M, _N>> {
+	using _Myt = _Input_layer;
+public:
+	using value_type = _Ty;
+	using input_t = Matrix_<_Ty, _M, _N>;
+	using category = typename _Layer_tag::input;
 
-		_Input_layer() noexcept {}
-		_Input_layer(const _Myt&) noexcept {}
+	_Input_layer(const _Myt& _Other) noexcept
+		: _Mydata(_Other._Mydata) {
+	}
+	_Input_layer(const input_t& _Input) noexcept 
+		: _Mydata(_Input) {
+	}
 
-		template<typename _Inty>
-		_Input_layer(const _Inty&) noexcept {}
+	/**
+	 *\brief Get rows of the input feature matrix. 
+	 */
+	MATRICE_HOST_INL constexpr auto rows() const noexcept {
+		return _Mydata.rows();
+	}
+	/**
+	 *\brief Get rows of the input feature matrix.
+	 */
+	MATRICE_HOST_INL constexpr auto cols() const noexcept {
+		return _Mydata.cols();
+	}
 
-	};
+	MATRICE_HOST_INL decltype(auto) data() const noexcept {
+		return (_Mydata);
+	}
+	MATRICE_HOST_INL decltype(auto) data() noexcept {
+		return (_Mydata);
+	}
+private:
+	input_t _Mydata;
+};
+template<typename _Ty, int _N>
+struct _Layer_traits<_Input_layer<Matrix_<_Ty, 1, _N>>>
+{
+	static constexpr auto depth = 1;
+	static constexpr auto insize = _N;
+	static constexpr auto outsize = _N;
+	static constexpr auto has_bias = false;
+	using value_type = _Ty;
+};
 
-	template<typename _Ty, int _M, int _N, size_t _D>
-	class _Input_layer<std::array<Matrix_<_Ty, _M, _N>, _D>> {
-		using _Myt = _Input_layer;
-	public:
-		using input_type = std::array<Matrix_<_Ty, _M, _N>, _D>;
-		using category = typename _Layer_tag::input;
 
-		_Input_layer() noexcept {}
-		_Input_layer(const _Myt&) noexcept {}
+template<typename _Ty, int _M, int _N, size_t _D>
+class _Input_layer<std::array<Matrix_<_Ty, _M, _N>, _D>> {
+	using _Myt = _Input_layer;
+public:
+	using input_t = std::array<Matrix_<_Ty, _M, _N>, _D>;
+	using category = typename _Layer_tag::input;
 
-		template<typename _Inty>
-		_Input_layer(const _Inty&) noexcept {}
+	_Input_layer() noexcept {}
+	_Input_layer(const _Myt&) noexcept {}
 
-	};
-}
-DGE_MATRICE_END
+	template<typename _Inty>
+	_Input_layer(const _Inty&) noexcept {}
+
+private:
+	input_t _Mydata;
+};
+} // namespace detail
+
+template<typename _Input>
+using input_layer = detail::_Input_layer<_Input>;
+
+MATRICE_NAMESPACE_END(dnn)
