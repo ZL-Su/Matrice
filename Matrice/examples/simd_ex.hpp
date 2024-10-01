1ced836706bbd74b4c6eca28a6961970fd3b8e29
ZL-Su <su-zl@seu.edu.cn>
2024-10-01 18:41:08 +0800

    Update simd_ex.hpp

--- Matrice/examples/simd_ex.hpp
+++ Matrice/examples/simd_ex.hpp
@@ -1,6 +1,6 @@
 /*********************************************************************
 This file is part of Matrice, an effcient and elegant C++ library.
-Copyright(C) 2018-2021, Zhilong(Dgelom) Su, all rights reserved.
+Copyright(C) 2018-2023, Zhilong(Dgelom) Su, all rights reserved.
 
 This program is free software : you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -27,14 +27,12 @@ namespace example {
 	auto c = decltype(b){};
 	
 	// Alias of SIMD type.
-	using packet_t = simd::Packet_<float>;
+	using packet_t = simd::Packet_<double>;
 
 	constexpr index_t packed_size = packet_t::size;
 
 	packet_t add(const packet_t& _Left, const packet_t _Right) {
 		return _Left + _Right;
 	}
-	
-	
 }
 DGE_MATRICE_END
