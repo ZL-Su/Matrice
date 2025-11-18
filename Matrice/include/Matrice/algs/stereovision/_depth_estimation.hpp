a7b278eb11245d800f75698ab1d7071fb00f7ebe
zl-su <su-zl@seu.edu.cn>
2025-11-18 21:41:09 +0800

    Update copyright year in _depth_estimation.hpp
    Updated copyright year from 2023 to 2025.

--- Matrice/include/Matrice/algs/stereovision/_depth_estimation.hpp
+++ Matrice/include/Matrice/algs/stereovision/_depth_estimation.hpp
@@ -1,6 +1,6 @@
 /*********************************************************************
 This file is part of Matrice, an effcient and elegant C++ library.
-Copyright(C) 2018-2023, Zhilong(Dgelom) Su, all rights reserved.
+Copyright(C) 2018-2025, Zhilong(Dgelom) Su, all rights reserved.
 
 This program is free software : you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
